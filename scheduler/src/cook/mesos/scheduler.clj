7ba84e1867fbe15d66437fde6791e97a34384c54
David Greenberg <dsg123456789@gmail.com>
2015-09-30 10:05:38 -0500

    Only kill nonterminal tasks instead of only not killing lost tasks

--- scheduler/src/cook/mesos/scheduler.clj
+++ scheduler/src/cook/mesos/scheduler.clj
@@ -210,10 +210,14 @@
                                           job-resources
                                           instance-runtime))
              ;; This code kills any task that "shouldn't" be running
-             (when (or (nil? instance) ; We could know nothing about the task, meaning a DB error happened and it's a waste to finish
-                       (not= task-state :task-lost) ; killing an unknown task causes a TASK_LOST message. Break the cycle!
-                       (= prior-job-state :job.state/completed) ; The task is attached to a failed job, possibly due to instances running on multiple hosts
-                       (= prior-instance-status :instance.status/failed)) ; The kill-task message could've been glitched on the network
+             (when (and
+                     (or (nil? instance) ; We could know nothing about the task, meaning a DB error happened and it's a waste to finish
+                         (= prior-job-state :job.state/completed) ; The task is attached to a failed job, possibly due to instances running on multiple hosts
+                         (= prior-instance-status :instance.status/failed)) ; The kill-task message could've been glitched on the network
+                     (contains? #{:task-running
+                                  :task-staging
+                                  :task-starting}
+                                task-state)) ; killing an unknown task causes a TASK_LOST message. Break the cycle! Only kill non-terminal tasks
                (log/warn "Attempting to kill task" task-id
                          "as instance" instance "with" prior-job-state "and" prior-instance-status
                          "should've been put down already")
