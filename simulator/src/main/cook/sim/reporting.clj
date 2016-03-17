2be5d01c6edb2f2deebb8988a348e0ff294c8ddd
Matthew Forsyth <matt@mojotech.com>
2016-03-17 13:48:15 -0400

    simulator: If any jobs weren't scheduled, describe the situation.

--- simulator/src/main/cook/sim/reporting.clj
+++ simulator/src/main/cook/sim/reporting.clj
@@ -22,22 +22,50 @@
        db uuid))
 
 (defn wait-time
+  [scheduled-at first-cook-instance]
+  (if first-cook-instance
+    (- (.getTime (:instance/start-time first-cook-instance))
+       (.getTime scheduled-at))))
+
+(defn job-result
   [cook-db [job-id action-at]]
-  ;; TODO: better exception reporting in order to point the way to identifying
-  ;; that the jobs weren't actually run by cook.
-  (- (.getTime (-> (pull-job-entity cook-db job-id)
-                   :job/instance first :instance/start-time))
-     (.getTime action-at)))
+  (let [cook-job (pull-job-entity cook-db job-id)
+        first-instance (-> cook-job :job/instance first)]
+    {:id job-id
+     :details cook-job
+     :wait-time (wait-time action-at first-instance)}))
+
+(defn warn-about-unscheduled
+  [jobs]
+  (let [unscheduled-jobs (remove :wait-time jobs)]
+    (if (-> unscheduled-jobs count pos?)
+      (do
+       (println "Warning!  This simulation had jobs that were never scheduled by Cook.")
+       (println "This could be because you are analyzing before the jobs had a chance to finish...")
+       (println "or it could mean that there's a problem with Cook,")
+       (println "or with the Simulator's ability to connect to Cook.")
+       (doseq [job unscheduled-jobs]
+         (println "Job" (:id job))
+         (println "details from Cook DB:" (:details job)))))))
+
+(defn show-average-wait
+  [jobs]
+  (let [wait-times (remove nil? (map :wait-time jobs))]
+    (if (-> wait-times count pos?)
+      (let [avg-wait (/ (apply + wait-times) (count wait-times))]
+        (println "Average wait time for jobs is" (float (/ avg-wait 1000)) "seconds."))
+      (println "No jobs were scheduled; thus, there is no average wait time."))))
 
 (defn analyze
   [settings sim-db cook-db sim-id]
   (println "Analyzing sim " sim-id "...")
   (let [cook-db-val (-> cook-db :conn d/db)
         sim-db-val (-> sim-db :conn d/db)
-        millis-for-jobs (map (partial wait-time cook-db-val)
-                             (jobs-for-sim sim-db-val sim-id))
-        avg-millis (/ (apply + millis-for-jobs) (count millis-for-jobs))]
-    (println "Average wait time for jobs is" (float (/ avg-millis 1000)) "seconds.")))
+        job-results (map (partial job-result cook-db-val)
+                         (jobs-for-sim sim-db-val sim-id))]
+    (println (count job-results) "jobs in sim...")
+    (warn-about-unscheduled job-results)
+    (show-average-wait job-results)))
 
 (defn- format-sim-result
   "Prepares a simulation id result from db/pull for output via print-table"
