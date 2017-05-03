b71d06fd9cc2092dea1bee16a91fdccc84cd5843
Shams Imam <shams.imam@twosigma.com>
2017-05-03 17:17:29 -0500

    travis: display executor logs before entire cook log

--- travis/show_executor_logs.sh
+++ travis/show_executor_logs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -ev
 
-cat $PROJECT_DIR/../scheduler/log/cook.log | grep -C 5 "error\|exception"
-
 echo "Printing out all executor logs..."
 while read path; do
-    cat "$path"
+    echo "Contents of ${path}";
+    cat "${path}";
+    echo "------------------------------------"
 done <<< "$(find $PROJECT_DIR/../travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
+
+echo "Contents of scheduler/log/cook.log"
+cat $PROJECT_DIR/../scheduler/log/cook.log
+echo "------------------------------------"
