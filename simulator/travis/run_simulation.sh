3b4d1935fd1098744fde3bf68ab7bdec793c2d71
Shams Imam <shams.imam@twosigma.com>
2017-05-03 17:14:18 -0500

    simulator: display logs only during error

--- simulator/travis/run_simulation.sh
+++ simulator/travis/run_simulation.sh
@@ -7,10 +7,13 @@ $PROJECT_DIR/../travis/start_scheduler.sh
 cd $PROJECT_DIR
 lein run -c config/settings.edn setup-database -c travis/simulator_config.edn
 
-set e
+set +e
 lein run -c config/settings.edn travis -c travis/simulator_config.edn
 SIM_EXIT_CODE=$?
 
-$PROJECT_DIR/../travis/show_executor_logs.sh
+if [ ${SIM_EXIT_CODE} -ne 0 ]; then
+  echo "Displaying executor logs"
+  $PROJECT_DIR/../travis/show_executor_logs.sh
+fi
 
-exit $SIM_EXIT_CODE
+exit ${SIM_EXIT_CODE}
