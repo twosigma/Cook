0e76644ae90d18cd5b763a0a20876614db213c02
Shams <shamsimam@users.noreply.github.com>
2017-05-04 09:53:12 -0500

    Launch scripts refactor (#318)
    * simulator: display logs only during error
    
    * integration: display logs only during error
    
    * travis: upgrade to minimesos 0.13.0
    
    * travis: display executor logs before entire cook log

--- integration/travis/run_integration.sh
+++ integration/travis/run_integration.sh
@@ -4,7 +4,14 @@ set -ev
 export PROJECT_DIR=`pwd`
 $PROJECT_DIR/../travis/start_scheduler.sh
 
+set +e
 cd $PROJECT_DIR
 python setup.py nosetests
+TESTS_EXIT_CODE=$?
 
-$PROJECT_DIR/../travis/show_executor_logs.sh
+if [ ${TESTS_EXIT_CODE} -ne 0 ]; then
+  echo "Displaying executor logs"
+  $PROJECT_DIR/../travis/show_executor_logs.sh
+fi
+
+exit ${TESTS_EXIT_CODE}
