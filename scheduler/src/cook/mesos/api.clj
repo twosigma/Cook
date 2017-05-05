5fc48bc58e0c14c29bf8879c45e4c028f9286f46
Daniel Posada <daniel.posada@twosigma.com>
2017-05-05 14:19:55 -0500

    Fixes bug

--- scheduler/src/cook/mesos/api.clj
+++ scheduler/src/cook/mesos/api.clj
@@ -850,7 +850,7 @@
                ::instances-requested instances}]
 
         (and allow-partial-results
-             (or (pos? existing-jobs)
+             (or (pos? (count existing-jobs))
                  (some some? instance-jobs)))
         [true {::jobs (into existing-jobs (filter some? instance-jobs))
                ::jobs-requested jobs
