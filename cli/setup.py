9c252f49fcc8884fd06ce9af19a89183d4983d94
Daniel Posada <daniel.posada@twosigma.com>
2017-05-26 13:56:31 -0500

    Adds install_requires to cli/setup.py

--- cli/setup.py
+++ cli/setup.py
@@ -2,13 +2,15 @@
 
 from setuptools import setup
 
+requirements = [
+    'requests',
+    'tabulate'
+]
+
 setup(
     name='cook',
     version='1.0',
     packages=['cook'],
-    entry_points={
-        'console_scripts': [
-            'cs = cook.__main__:main'
-        ]
-    }
+    entry_points={'console_scripts': ['cs = cook.__main__:main']},
+    install_requires=requirements
 )
