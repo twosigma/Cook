07a17159a57774de225595ca11f5060bff0b3c2e
Daniel Posada <daniel.posada@twosigma.com>
2017-06-08 10:04:52 -0500

    Switches from lsof to curl for checking connection to cook

--- integration/travis/wait-for-cook.sh
+++ integration/travis/wait-for-cook.sh
@@ -9,8 +9,9 @@
 
 COOK_PORT=${1:-12321}
 
-while ! lsof -Pi :${COOK_PORT} -sTCP:LISTEN -t >/dev/null;
+while ! curl -s localhost:${COOK_PORT} >/dev/null;
 do
     echo "$(date +%H:%M:%S) Cook is not listening on ${COOK_PORT} yet"
     sleep 2.0
 done
+echo "$(date +%H:%M:%S) Connected to Cook on ${COOK_PORT}!"
