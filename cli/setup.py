2a70261abc403ddc3e7987df898a2c0a530ada27
Daniel Posada <daniel.posada@gmail.com>
2017-10-03 13:11:48 -0500

    Adds packaging info for pypi (#475)

--- cli/setup.py
+++ cli/setup.py
@@ -16,8 +16,11 @@ test_requirements = [
 ]
 
 setup(
-    name='twosigma.cook-cli',
-    version='0.1.0a1',
+    name='cook_client',
+    version='0.1.0a4',
+    description="Two Sigma's Cook CLI",
+    long_description="This package contains Two Sigma's Cook Scheduler command line interface, cs. cs allows you to "
+                     "submit jobs and view jobs, job instances, and job groups across multiple Cook clusters.",
     packages=['cook', 'cook.subcommands'],
     entry_points={'console_scripts': ['cs = cook.__main__:main']},
     install_requires=requirements,
