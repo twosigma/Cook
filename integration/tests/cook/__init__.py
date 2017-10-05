d5c17167562a597882a4692d2da71139fae62842
Daniel Posada <daniel.posada@twosigma.com>
2017-10-05 16:35:00 -0500

    More integration testing

--- integration/tests/cook/__init__.py
+++ integration/tests/cook/__init__.py
@@ -1,4 +1,3 @@
 import logging
 
-logging.getLogger('').handlers = []
 logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
--- integration/tests/cook/test_cli.py
+++ integration/tests/cook/test_cli.py
@@ -504,12 +504,16 @@ class CookCliTest(unittest.TestCase):
         cp, jobs = cli.show_json(uuids, self.cook_url)
         self.assertEqual('cook-scheduler-cli', jobs[0]['application']['name'])
 
-    def test_list_connection_error(self):
+    def test_read_timeout_messaging(self):
         config = {'http': {'read-timeout': 0.001}}
         with cli.temp_config_file(config) as path:
             cp = cli.list_jobs(self.cook_url, flags=f'--config {path}')
             self.assertEqual(1, cp.returncode)
             self.assertIn('Encountered connection error', cli.decode(cp.stderr))
+            cp, uuids = cli.submit('ls', self.cook_url, flags=f'--config {path}')
+            self.assertEqual(1, cp.returncode)
+            self.assertIn('Encountered read timeout', cli.stdout(cp))
+            self.assertIn('Your submission may have completed', cli.stdout(cp))
 
     def test_list_invalid_flags(self):
         error_fragment = 'cannot specify both lookback hours and submitted after / before times'
