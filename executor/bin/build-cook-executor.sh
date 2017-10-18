7d6314d06cf68ac4a524618304e8b58077010d25
Nick Vrvilo <Nick.Vrvilo@twosigma.com>
2017-10-18 09:31:14 -0500

    Delete docker containers automatically after running

--- executor/bin/build-cook-executor.sh
+++ executor/bin/build-cook-executor.sh
@@ -5,18 +5,13 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NAME=cook-executor-build
 
-if [ "$(docker ps -aq -f name=${NAME})" ]; then
-    # Cleanup
-    docker rm ${NAME}
-fi
-
 EXECUTOR_DIR="$(dirname ${DIR})"
 
 mkdir -p ${EXECUTOR_DIR}/dist
 
 # build cook-executor inside docker image to avoid local python environment and architecture hassles
-pushd ${EXECUTOR_DIR}
+cd ${EXECUTOR_DIR}
 docker build -t ${NAME} -f ${EXECUTOR_DIR}/Dockerfile.build .
 docker run --name ${NAME} ${NAME}
 docker cp ${NAME}:/opt/cook/dist/cook-executor ${EXECUTOR_DIR}/dist/cook-executor
-popd
+docker rm ${NAME}
