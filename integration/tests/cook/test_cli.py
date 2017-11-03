daf21acad72716bd3ec20920f0bddeaa90a9b112
Shams Imam <shams.imam@twosigma.com>
2017-11-03 17:10:30 -0500

    addresses feedback from dposada by - introducing __wait_for_executor_completion_message()

--- integration/tests/cook/test_cli.py
+++ integration/tests/cook/test_cli.py
@@ -882,6 +882,10 @@ class CookCliTest(unittest.TestCase):
         return util.wait_until(lambda: cli.show_json(uuids, self.cook_url)[1][0]['instances'][0],
                                lambda i: 'progress' in i and 'progress_message' in i)
 
+    def __wait_for_executor_completion_message(self, uuids):
+        return util.wait_until(lambda: cli.decode(cli.tail(uuids[0], 'stdout', self.cook_url, '--lines 100').stdout),
+                               lambda out: 'Executor completed execution' in out)
+
     def test_show_progress_message(self):
         executor = util.get_job_executor_type(self.cook_url)
         cp, uuids = cli.submit('echo "progress: 99 We are so close!"', self.cook_url,
@@ -900,14 +904,11 @@ class CookCliTest(unittest.TestCase):
             self.assertEqual(0, cp.returncode, cp.stderr)
             self.assertIn("99% We are so close!", cli.stdout(cp))
 
-            self.logger.info('Sleeping 5 seconds to allow cook executor to complete execution')
-            time.sleep(5.0) # allow the executor to wrap up
-
-            cp = cli.tail(uuids[0], 'stdout', self.cook_url, '--lines 100')
-            self.logger.debug(f'Contents of stdout: {cli.decode(cp.stdout)}')
-            self.assertIn("99 We are so close!", cli.decode(cp.stdout))
-            self.assertIn('Command exited with status 0', cli.decode(cp.stdout))
-            self.assertIn('Executor completed execution', cli.decode(cp.stdout))
+            stdout = self.__wait_for_executor_completion_message(uuids)
+            self.logger.debug(f'Contents of stdout: {stdout}')
+            self.assertIn("99 We are so close!", stdout)
+            self.assertIn('Command exited with status 0', stdout)
+            self.assertIn('Executor completed execution', stdout)
 
     def test_show_progress_message_custom_progress_file(self):
         executor = util.get_job_executor_type(self.cook_url)
@@ -932,13 +933,10 @@ class CookCliTest(unittest.TestCase):
             cp = cli.show(uuids, self.cook_url)
             self.assertEqual(0, cp.returncode, cp.stderr)
 
-            self.logger.info('Sleeping 5 seconds to allow cook executor to complete execution')
-            time.sleep(5.0) # allow the executor to wrap up
-
-            cp = cli.tail(uuids[0], 'stdout', self.cook_url, '--lines 100')
-            self.logger.debug(f'Contents of stdout: {cli.decode(cp.stdout)}')
-            self.assertIn('Command exited with status 0', cli.decode(cp.stdout))
-            self.assertIn('Executor completed execution', cli.decode(cp.stdout))
+            stdout = self.__wait_for_executor_completion_message(uuids)
+            self.logger.debug(f'Contents of stdout: {stdout}')
+            self.assertIn('Command exited with status 0', stdout)
+            self.assertIn('Executor completed execution', stdout)
 
     def test_submit_with_executor(self):
         cp, uuids = cli.submit('ls', self.cook_url, submit_flags='--executor cook')
