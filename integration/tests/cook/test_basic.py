540188fec304ace9ad732391f91c0e2fa5d194ff
Paul Schorfheide <paul.schorfheide@twosigma.com>
2017-11-16 08:19:39 -0600

    add logging

--- integration/tests/cook/test_basic.py
+++ integration/tests/cook/test_basic.py
@@ -1332,7 +1332,9 @@ class CookTest(unittest.TestCase):
                 resp = util.unscheduled_jobs(self.cook_url, waiting_job['uuid'])[0][0]
                 placement_reasons = [reason for reason in resp['reasons']
                                      if reason['reason'] == reasons.COULD_NOT_PLACE_JOB]
+                jobs = util.query_jobs(self.cook_url, job=uuids).json()
                 self.logger.info(f"unscheduled_jobs response: {resp}")
+                self.logger.info(f"jobs: {jobs}")
                 return placement_reasons
 
             placement_reasons = util.wait_until(query_unscheduled, lambda r: len(r) > 0)
