106985baf0c01cbde7531ae7a1ce9fce9878956b
Shams Imam <shams.imam@twosigma.com>
2017-11-20 14:34:02 -0600

    addresses feedback from dposada by  - adding comments to the use of one-folder pyinstaller mode

--- executor/bin/build-local.sh
+++ executor/bin/build-local.sh
@@ -13,5 +13,7 @@ EXECUTOR_DIR="$(dirname ${DIR})"
 mkdir -p ${EXECUTOR_DIR}/dist
 rm -rf ${EXECUTOR_DIR}/dist/cook-executor-local
 
+# Create a one-folder bundle containing an executable (instead of using the one-file version).
+# Allows us to avoid the extraction to a temporary folder needed by the PyInstaller bootloader.
 cd ${EXECUTOR_DIR}
 pyinstaller --onedir --name cook-executor-local --paths cook cook/__main__.py
