4f7249b862f8b3b5d702546a40b3766764d87950
Shams <shamsimam@users.noreply.github.com>
2017-12-07 14:42:16 -0600

    Mimic Mesos CommandExecutor kill logic of killing process tree (#624)

--- executor/cook/executor.py
+++ executor/cook/executor.py
@@ -1,17 +1,16 @@
-#!/usr/bin/env python3
-
 import json
 import logging
-import os
 import signal
-import subprocess
 import time
 from threading import Event, Thread, Timer
+
+import os
+import pymesos as pm
 
 import cook
 import cook.io_helper as cio
 import cook.progress as cp
-import pymesos as pm
+import cook.subprocess as cs
 
 
 def get_task_id(task):
@@ -117,37 +116,10 @@ def launch_task(task, environment):
         data_json = json.loads(data_string)
         command = str(data_json['command']).strip()
         logging.info('Command: {}'.format(command))
-        if not command:
-            logging.warning('No command provided!')
-            return None
-
-        # The preexec_fn is run after the fork() but before exec() to run the shell.
-        process = subprocess.Popen(command,
-                                   env=environment,
-                                   preexec_fn=os.setpgrp,
-                                   shell=True,
-                                   stderr=subprocess.PIPE,
-                                   stdout=subprocess.PIPE)
-
-        return process
+        return cs.launch_process(command, environment)
     except Exception:
         logging.exception('Error in launch_task')
         return None
-
-
-def is_process_running(process):
-    """Checks whether the process is still running.
-
-    Parameters
-    ----------
-    process: subprocess.Popen
-        The process to query
-
-    Returns
-    -------
-    whether the process is still running.
-    """
-    return process.poll() is None
 
 
 def is_running(process_info):
@@ -168,87 +140,7 @@ def is_running(process_info):
     whether any of the process or the piping threads are still running.
     """
     process, stdout_thread, stderr_thread = process_info
-    return is_process_running(process) or stdout_thread.isAlive() or stderr_thread.isAlive()
-
-
-def find_process_group(process_id):
-    """Return the process group id of the process with process id process_id.
-    Parameters
-    ----------
-    process_id: int
-        The process id.
-    Returns
-    -------
-    The process group id of the process with process id process_id or None.
-    """
-    try:
-        group_id = os.getpgid(process_id)
-        logging.info('Process (pid: {}) belongs to group (id: {})'.format(process_id, group_id))
-        return group_id
-    except ProcessLookupError:
-        logging.info('Unable to find group for process (pid: {})'.format(process_id))
-    except Exception:
-        logging.exception('Error in finding group for process (pid: {})'.format(process_id))
-
-
-def kill_process_group(group_id, signal_to_send):
-    """Send the SIGKILL signal to the process group with group_id.
-    Parameters
-    ----------
-    group_id: int
-        The id of the process group.
-    signal_to_send: signal.Signals enum
-        The signal to send to the process group.
-    Returns
-    -------
-    Nothing
-    """
-    signal_name = signal_to_send.name
-    try:
-        if group_id:
-            logging.info('Sending signal {} to group (id: {})'.format(signal_name, group_id))
-            os.killpg(group_id, signal_to_send)
-    except ProcessLookupError:
-        logging.info('Unable to send signal {} as could not find group (id: {})'.format(signal_name, group_id))
-    except Exception:
-        logging.exception('Error in sending signal {} to group (id: {})'.format(signal_name, group_id))
-
-
-def kill_task(process, shutdown_grace_period_ms):
-    """Attempts to kill a process.
-     First attempt is made by sending the process a SIGTERM.
-     If the process does not terminate inside (shutdown_grace_period_ms - 100) ms, it is then sent a SIGKILL.
-     The 100 ms grace period is allocated for the executor to perform its other cleanup actions.
-
-    Parameters
-    ----------
-    process: subprocess.Popen
-        The process to kill
-    shutdown_grace_period_ms: int
-        Grace period before forceful kill
-
-    Returns
-    -------
-    Nothing
-    """
-    shutdown_grace_period_ms = max(shutdown_grace_period_ms - (1000 * cook.TERMINATE_GRACE_SECS), 0)
-    if is_process_running(process):
-        logging.info('Waiting up to {} ms for process to terminate'.format(shutdown_grace_period_ms))
-        group_id = find_process_group(process.pid)
-        kill_process_group(group_id, signal.SIGTERM)
-
-        loop_limit = int(shutdown_grace_period_ms / 10)
-        for i in range(loop_limit):
-            time.sleep(0.01)
-            if not is_process_running(process):
-                cio.print_and_log('Command terminated with signal Terminated (pid: {})'.format(process.pid),
-                                  flush=True)
-                break
-        if is_process_running(process):
-            logging.info('Process did not terminate, forcefully killing it')
-            kill_process_group(group_id, signal.SIGKILL)
-            cio.print_and_log('Command terminated with signal Killed (pid: {})'.format(process.pid),
-                              flush=True)
+    return cs.is_process_running(process) or stdout_thread.isAlive() or stderr_thread.isAlive()
 
 
 def await_process_completion(stop_signal, process_info, shutdown_grace_period_ms):
@@ -277,7 +169,7 @@ def await_process_completion(stop_signal, process_info, shutdown_grace_period_ms
         if stop_signal.isSet():
             logging.info('Executor has been instructed to terminate running task')
             process, _, _ = process_info
-            kill_task(process, shutdown_grace_period_ms)
+            cs.kill_process(process, shutdown_grace_period_ms)
             break
 
         time.sleep(cook.RUNNING_POLL_INTERVAL_SECS)
@@ -352,7 +244,7 @@ def manage_task(driver, task, stop_signal, completed_signal, config):
     -------
     Nothing
     """
-    group_id = None
+    launched_process = None
     task_id = get_task_id(task)
     cio.print_and_log('Starting task {}'.format(task_id))
     try:
@@ -365,22 +257,19 @@ def manage_task(driver, task, stop_signal, completed_signal, config):
         send_message(driver, sandbox_message, config.max_message_length)
 
         environment = retrieve_process_environment(config, os.environ)
-        process = launch_task(task, environment)
-        if process:
+        launched_process = launch_task(task, environment)
+        if launched_process:
             # task has begun running successfully
             update_status(driver, task_id, cook.TASK_RUNNING)
-            cio.print_and_log('Forked command at {}'.format(process.pid))
+            cio.print_and_log('Forked command at {}'.format(launched_process.pid))
         else:
             # task launch failed, report an error
             logging.error('Error in launching task')
             update_status(driver, task_id, cook.TASK_ERROR)
             return
 
-        group_id = find_process_group(process.pid)
-
-        flush_interval_secs = config.flush_interval_secs
-        max_bytes_read_per_line = config.max_bytes_read_per_line
-        stdout_thread, stderr_thread = cio.track_outputs(task_id, process, flush_interval_secs, max_bytes_read_per_line)
+        stdout_thread, stderr_thread = cio.track_outputs(task_id, launched_process, config.flush_interval_secs,
+                                                         config.max_bytes_read_per_line)
         task_completed_signal = Event() # event to track task execution completion
         sequence_counter = cp.ProgressSequenceCounter()
 
@@ -405,7 +294,7 @@ def manage_task(driver, task, stop_signal, completed_signal, config):
         logging.info('Progress will be tracked from {} locations'.format(len(progress_locations)))
         progress_trackers = [launch_progress_tracker(l, progress_locations[l]) for l in progress_locations]
 
-        process_info = process, stdout_thread, stderr_thread
+        process_info = launched_process, stdout_thread, stderr_thread
         await_process_completion(stop_signal, process_info, config.shutdown_grace_period_ms)
         task_completed_signal.set()
 
@@ -414,8 +303,8 @@ def manage_task(driver, task, stop_signal, completed_signal, config):
         progress_termination_timer.start()
 
         # propagate the exit code
-        exit_code = process.returncode
-        cio.print_and_log('Command exited with status {} (pid: {})'.format(exit_code, process.pid),
+        exit_code = launched_process.returncode
+        cio.print_and_log('Command exited with status {} (pid: {})'.format(exit_code, launched_process.pid),
                           flush=True)
 
         exit_message = json.dumps({'exit-code': exit_code, 'task-id': task_id})
@@ -444,7 +333,8 @@ def manage_task(driver, task, stop_signal, completed_signal, config):
     finally:
         # ensure completed_signal is set so driver can stop
         completed_signal.set()
-        kill_process_group(group_id, signal.SIGKILL)
+        if launched_process and cs.is_process_running(launched_process):
+            cs.send_signal(launched_process.pid, signal.SIGKILL)
 
 
 class CookExecutor(pm.Executor):
@@ -477,7 +367,7 @@ class CookExecutor(pm.Executor):
         config = self.config
 
         task_thread = Thread(target=manage_task, args=(driver, task, stop_signal, completed_signal, config))
-        task_thread.daemon= True
+        task_thread.daemon = True
         task_thread.start()
 
     def killTask(self, driver, task_id):
---
+++ executor/cook/subprocess.py
@@ -0,0 +1,233 @@
+import logging
+import signal
+import subprocess
+import time
+
+import os
+import psutil
+
+import cook
+import cook.io_helper as cio
+
+
+def launch_process(command, environment):
+    """Launches the process using the command and specified environment.
+
+    Parameters
+    ----------
+    command: string
+        The command to execute.
+    environment: dictionary
+        The environment.
+
+    Returns
+    -------
+    The launched process.
+    """
+    if not command:
+        logging.warning('No command provided!')
+        return None
+    # The preexec_fn is run after the fork() but before exec() to run the shell.
+    # setsid will run the program in a new session, thus assigning a new process group to it and its children.
+    return subprocess.Popen(command,
+                            env=environment,
+                            preexec_fn=os.setsid,
+                            shell=True,
+                            stderr=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
+
+
+def is_process_running(process):
+    """Checks whether the process is still running.
+
+    Parameters
+    ----------
+    process: subprocess.Popen
+        The process to query
+
+    Returns
+    -------
+    whether the process is still running.
+    """
+    return process.poll() is None
+
+
+def find_process_group(process_id):
+    """Return the process group id of the process with process id process_id.
+    Parameters
+    ----------
+    process_id: int
+        The process id.
+    Returns
+    -------
+    The process group id of the process with process id process_id or None.
+    """
+    try:
+        group_id = os.getpgid(process_id)
+        logging.info('Process (pid: {}) belongs to group (id: {})'.format(process_id, group_id))
+        return group_id
+    except ProcessLookupError:
+        logging.info('Unable to find group for process (pid: {})'.format(process_id))
+    except Exception:
+        logging.exception('Error in finding group for process (pid: {})'.format(process_id))
+
+
+def _send_signal_to_process(process_id, signal_to_send):
+    """Send the signal_to_send signal to the process with process_id.
+    Parameters
+    ----------
+    process_id: int
+        The id of the process whose group to kill.
+    signal_to_send: signal.Signals enum
+        The signal to send to the process group.
+    Returns
+    -------
+    True if the signal was sent successfully.
+    """
+    signal_name = signal_to_send.name
+    try:
+        logging.info('Sending {} to process (id: {})'.format(signal_name, process_id))
+        os.kill(process_id, signal_to_send)
+        return True
+    except ProcessLookupError:
+        logging.info('Unable to send {} as could not find process (id: {})'.format(signal_name, process_id))
+    except Exception:
+        logging.exception('Error in sending {} to process (id: {})'.format(signal_name, process_id))
+    return False
+
+
+def _send_signal_to_process_tree(root_process_id, signal_to_send):
+    """Send the signal_to_send signal to the process tree rooted at process_id.
+    Parameters
+    ----------
+    process_id: int
+        The id of the root process.
+    signal_to_send: signal.Signals enum
+        The signal to send to the process group.
+    Returns
+    -------
+    True if the signal was sent successfully.
+    """
+    signal_name = signal_to_send.name
+    logging.info('Sending {} to process tree rooted at (id: {})'.format(signal_name, root_process_id))
+
+    process_queue = [root_process_id]
+    visited_process_ids = set()
+
+    num_processes_found = 0
+    num_processes_killed = 0
+    signal_sent_to_all_processes_successfully = True
+
+    while process_queue:
+        loop_process_id = process_queue.pop(0)
+        num_processes_found += 1
+        try:
+            visited_process_ids.add(loop_process_id)
+
+            # Stop the process to keep it from forking since a forked child might get re-parented
+            _send_signal_to_process(loop_process_id, signal.SIGSTOP)
+
+            # Now safe to retrieve the children
+            process = psutil.Process(loop_process_id)
+            children = process.children()
+            [process_queue.append(child.pid) for child in children if child.pid not in visited_process_ids]
+
+            if _send_signal_to_process(loop_process_id, signal_to_send):
+                num_processes_killed += 1
+            else:
+                signal_sent_to_all_processes_successfully = False
+        except ProcessLookupError:
+            logging.info('Unable to send {} as could not find process (id: {})'.format(signal_name, loop_process_id))
+        except Exception:
+            logging.exception('Error in sending {} to process (id: {})'.format(signal_name, loop_process_id))
+            signal_sent_to_all_processes_successfully = False
+
+    log_message = 'Found {} processes in tree rooted at (id: {}), successfully sent {} to {} processes'
+    logging.info(log_message.format(num_processes_found, root_process_id, signal_name, num_processes_killed))
+
+    for loop_process_id in visited_process_ids:
+        # Try and continue the processes in case the signal is non-terminating but doesn't continue the process.
+        _send_signal_to_process(loop_process_id, signal.SIGCONT)
+
+    return signal_sent_to_all_processes_successfully
+
+
+def _send_signal_to_process_group(process_id, signal_to_send):
+    """Send the signal_to_send signal to the process group with group_id.
+    Parameters
+    ----------
+    process_id: int
+        The id of the process whose group to kill.
+    signal_to_send: signal.Signals enum
+        The signal to send to the process group.
+    Returns
+    -------
+    True if the signal was sent successfully.
+    """
+    signal_name = signal_to_send.name
+    try:
+        group_id = find_process_group(process_id)
+        if group_id:
+            logging.info('Sending {} to group (id: {})'.format(signal_name, group_id))
+            os.killpg(group_id, signal_to_send)
+            return True
+    except ProcessLookupError:
+        logging.info('Unable to send {} as could not find group (id: {})'.format(signal_name, group_id))
+    except Exception:
+        logging.exception('Error in sending {} to group (id: {})'.format(signal_name, group_id))
+    return False
+
+
+def send_signal(process_id, signal_to_send):
+    """Send the signal_to_send signal to the process with process_id.
+    The function uses a three-step mechanism:
+    1. It sends the signal to the process tree rooted at process_id;
+    2. If unsuccessful, it sends the signal to the process group of process_id;
+    3. If unsuccessful, it sends the signal directly to the process with id process_id."""
+    if process_id:
+        signal_name = signal_to_send.name
+        logging.info('Requested to send {} to process (id: {})'.format(signal_name, process_id))
+        if _send_signal_to_process_tree(process_id, signal_to_send):
+            logging.info('Successfully sent {} to process tree (id: {})'.format(signal_name, process_id))
+        elif _send_signal_to_process_group(process_id, signal_to_send):
+            logging.info('Successfully sent {} to group for process (id: {})'.format(signal_name, process_id))
+        elif _send_signal_to_process(process_id, signal_to_send):
+            logging.info('Successfully sent {} to process (id: {})'.format(signal_name, process_id))
+        else:
+            logging.info('Failed to send {} to process (id: {})'.format(signal_name, process_id))
+
+
+def kill_process(process, shutdown_grace_period_ms):
+    """Attempts to kill a process.
+     First attempt is made by sending the process a SIGTERM.
+     If the process does not terminate inside (shutdown_grace_period_ms - 100) ms, it is then sent a SIGKILL.
+     The 100 ms grace period is allocated for the executor to perform its other cleanup actions.
+
+    Parameters
+    ----------
+    process: subprocess.Popen
+        The process to kill
+    shutdown_grace_period_ms: int
+        Grace period before forceful kill
+
+    Returns
+    -------
+    Nothing
+    """
+    shutdown_grace_period_ms = max(shutdown_grace_period_ms - (1000 * cook.TERMINATE_GRACE_SECS), 0)
+    if is_process_running(process):
+        logging.info('Waiting up to {} ms for process to terminate'.format(shutdown_grace_period_ms))
+        send_signal(process.pid, signal.SIGTERM)
+
+        loop_limit = int(shutdown_grace_period_ms / 10)
+        for i in range(loop_limit):
+            time.sleep(0.01)
+            if not is_process_running(process):
+                cio.print_and_log('Command terminated with signal Terminated (pid: {})'.format(process.pid),
+                                  flush=True)
+                break
+        if is_process_running(process):
+            logging.info('Process did not terminate, forcefully killing it')
+            send_signal(process.pid, signal.SIGKILL)
+            cio.print_and_log('Command terminated with signal Killed (pid: {})'.format(process.pid),
+                              flush=True)
