51adf938692801dcdbabee837a11530c0a3e1a9b
Daniel Posada <daniel.posada@twosigma.com>
2018-01-10 10:48:16 -0600

    Reverts changes that were moved to PR #674

--- travis/show_executor_logs.sh
+++ travis/show_executor_logs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -v
 
+echo "what logs are there"
+find $PROJECT_DIR/../scheduler/log -name 'cook*.log'
 echo "Printing out all executor logs..."
 while read path; do
     echo "Contents of ${path}";
     cat "${path}";
     echo "------------------------------------"
-done <<< "$(find ${TRAVIS_BUILD_DIR}/travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
+done <<< "$(find $PROJECT_DIR/../travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
 
-${TRAVIS_BUILD_DIR}/travis/show_scheduler_logs.sh
+for log in $PROJECT_DIR/../scheduler/log/cook*.log;
+do
+    echo "Contents of ${log}"
+    cat "${log}";
+    echo "------------------------------------"
+done
--- travis/show_scheduler_logs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for log in ${TRAVIS_BUILD_DIR}/scheduler/log/cook*.log;
-do
-    echo "Contents of ${log}"
-    cat "${log}";
-    echo "------------------------------------"
-done
