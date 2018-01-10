630409f68c78a56ec8247854e4cb4c4a066c2892
Daniel Posada <daniel.posada@twosigma.com>
2018-01-10 10:39:02 -0600

    Stops dumping sandbox logs when there are test failures

--- travis/show_executor_logs.sh
+++ travis/show_executor_logs.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 set -v
 
-echo "what logs are there"
-find $PROJECT_DIR/../scheduler/log -name 'cook*.log'
 echo "Printing out all executor logs..."
 while read path; do
     echo "Contents of ${path}";
     cat "${path}";
     echo "------------------------------------"
-done <<< "$(find $PROJECT_DIR/../travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
+done <<< "$(find ${TRAVIS_BUILD_DIR}/travis/.minimesos -name 'stdout' -o -name 'stderr' -o -name 'executor.log')"
 
-for log in $PROJECT_DIR/../scheduler/log/cook*.log;
-do
-    echo "Contents of ${log}"
-    cat "${log}";
-    echo "------------------------------------"
-done
+${TRAVIS_BUILD_DIR}/travis/show_scheduler_logs.sh
