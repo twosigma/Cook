909a0743b6d24204104e572376ace41bdc466ff9
Daniel Posada <daniel.posada@twosigma.com>
2018-01-31 13:43:45 -0600

    Clears old counters when a user 'leaves' a state

--- scheduler/src/cook/mesos/monitor.clj
+++ scheduler/src/cook/mesos/monitor.clj
@@ -59,8 +59,7 @@
     (->> (vals stats)
          (apply merge-with +)
          (assoc stats "all"))
-    {"all" (zipmap (conj (util/get-all-resource-types db) :jobs)
-                   (repeat 0.0))}))
+    {"all" {:cpus 0, :mem 0, :jobs 0}}))
 
 (defn- get-starved-job-stats
   "Return a map from starved users ONLY to their stats where a stats is a map
@@ -89,39 +88,56 @@
              (recur users starved-stats)))
          starved-stats)))))
 
-(defn- set-counter
+(defn- set-counter!
   "Sets the value of the counter to the new value.
    A data race is possible if two threads invoke this function concurrently."
   [counter value]
-  (->> counter
-       counters/value
-       (- value)
-       (counters/inc! counter)))
+  (let [amount-to-inc (- (int value) (counters/value counter))]
+    (counters/inc! counter amount-to-inc)))
 
-(defn- set-stats-counters
+(defn- clear-old-counters!
+  "Clears counters that were present on the previous iteration
+  but not in the current iteration. This avoids the situation
+  where a user's job changes state but the old state's counter
+  doesn't reflect the change."
+  [state stats state->previous-stats-atom]
+  (let [previous-stats (get @state->previous-stats-atom state)
+        previous-users (set (keys previous-stats))
+        current-users (set (keys stats))
+        users-to-clear (difference previous-users current-users)]
+    (run! (fn [user]
+            (run! (fn [[type _]]
+                    (-> [state user (name type)]
+                        counters/counter
+                        counters/clear!))
+                  (get previous-stats user)))
+          users-to-clear)))
+
+(defn- set-user-counters!
   "Sets counters for jobs with the given state, e.g. running, waiting and starved."
-  [db state stats]
-  (dorun
-    (->> stats
-         (add-aggregated-stats db)
-         (mapcat (fn [[user stats]]
-                   (map (fn [[type amount]]
-                          (-> [state user (name type)]
-                              counters/counter
-                              (set-counter amount)))
-                        stats))))))
+  [db state stats state->previous-stats-atom]
+  (clear-old-counters! state stats state->previous-stats-atom)
+  (swap! state->previous-stats-atom #(assoc % state stats))
+  (run!
+    (fn [[user stats]]
+      (run! (fn [[type amount]]
+              (-> [state user (name type)]
+                  counters/counter
+                  (set-counter! amount)))
+            stats))
+    (add-aggregated-stats db stats)))
 
-(defn set-user-counter
+(defn set-total-counter!
   "Given a state (e.g. starved) and a value, sets the corresponding counter."
   [state value]
   (-> [state "users"]
       counters/counter
-      (set-counter value)))
+      (set-counter! value)))
 
-(defn- set-user-stats-counters
+(defn set-stats-counters!
   "Queries the database for running and waiting jobs per user, and sets
   counters for running, waiting, starved, hungry and satisifed users."
-  [db]
+  [db state->previous-stats-atom]
   (let [running-stats (get-job-stats db :job.state/running)
         waiting-stats (get-job-stats db :job.state/waiting)
         starved-stats (get-starved-job-stats db running-stats waiting-stats)
@@ -130,13 +146,13 @@
         satisfied-users (difference running-users waiting-users)
         starved-users (set (keys starved-stats))
         hungry-users (difference waiting-users starved-users)]
-    (set-stats-counters db "running" running-stats)
-    (set-stats-counters db "waiting" waiting-stats)
-    (set-stats-counters db "starved" starved-stats)
-    (set-user-counter "total" (count (union running-users waiting-users)))
-    (set-user-counter "starved" (count starved-users))
-    (set-user-counter "hungry" (count hungry-users))
-    (set-user-counter "satisfied" (count satisfied-users))))
+    (set-user-counters! db "running" running-stats state->previous-stats-atom)
+    (set-user-counters! db "waiting" waiting-stats state->previous-stats-atom)
+    (set-user-counters! db "starved" starved-stats state->previous-stats-atom)
+    (set-total-counter! "total" (count (union running-users waiting-users)))
+    (set-total-counter! "starved" (count starved-users))
+    (set-total-counter! "hungry" (count hungry-users))
+    (set-total-counter! "satisfied" (count satisfied-users))))
 
 (defn start-collecting-stats
   "Starts a periodic timer to collect stats about running, waiting, and starved jobs per user.
@@ -145,9 +161,10 @@
   [mesos-conn & {:keys [interval]
                  :or {interval 20000}}]
   (log/info "Starting stats collection for scheduler.")
-  (chime-at (periodic/periodic-seq (time/now) (time/millis interval))
-            (fn [_]
-              (let [mesos-db (db mesos-conn)]
-                (set-user-stats-counters mesos-db)))
-            {:error-handler (fn [ex]
-                              (log/error ex "Setting user stats counters failed!"))}))
+  (let [state->previous-stats-atom (atom {})]
+    (chime-at (periodic/periodic-seq (time/now) (time/millis interval))
+              (fn [_]
+                (let [mesos-db (db mesos-conn)]
+                  (set-stats-counters! mesos-db state->previous-stats-atom)))
+              {:error-handler (fn [ex]
+                                (log/error ex "Setting user stats counters failed!"))})))
