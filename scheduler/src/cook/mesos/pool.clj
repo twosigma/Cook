44a9aa9fe84d0dbc6f4afeb2b0633e601c060e8c
Paul Schorfheide <paul.schorfheide@twosigma.com>
2018-04-10 13:30:30 -0500

    Convert pool query to function call

--- scheduler/src/cook/mesos/pool.clj
+++ scheduler/src/cook/mesos/pool.clj
@@ -1,6 +1,16 @@
 (ns cook.mesos.pool
   (:require [datomic.api :as d]))
 
+(defn check-pool
+  "Returns true if requesting-default-pool? and the entity does not have a pool
+   or the entity has a pool named pool-name"
+  [source eid relationship pool-name requesting-default-pool?]
+  (let [ent (d/entity source eid)
+        pool (relationship ent)]
+    (or (and (nil? pool)
+             requesting-default-pool?)
+        (= pool-name (:pool/name pool)))))
+
 (defn all-pools
   "Returns a list of Datomic entities corresponding
   to all of the currently defined pools."
