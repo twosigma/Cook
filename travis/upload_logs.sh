bb3ec2ab4935b4795a13bb5c489ecb29edc33b7a
Daniel Posada <daniel.posada@gmail.com>
2018-05-02 15:23:30 -0500

    Adds build number directory to tarball (#834)

--- travis/upload_logs.sh
+++ travis/upload_logs.sh
@@ -3,5 +3,5 @@
 cd ${TRAVIS_BUILD_DIR}
 
 tarball=./dump.txz
-tar -cJf $tarball --transform='s|\./[^/]*/\.*||' ./scheduler/log ./travis/.minimesos
+tar -cJf $tarball --transform="s|\./[^/]*/\.*|${TRAVIS_JOB_NUMBER}/|" ./scheduler/log ./travis/.minimesos
 ./travis/gdrive_upload "travis-${TRAVIS_JOB_NUMBER:-dump}" $tarball
