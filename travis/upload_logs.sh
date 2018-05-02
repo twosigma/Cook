35b49a8579e33afed8308b72902db5ad7841821e
Daniel Posada <daniel.posada@gmail.com>
2018-05-02 18:14:42 -0500

    Uploads the Mesos master logs on failures (#836)

--- travis/upload_logs.sh
+++ travis/upload_logs.sh
@@ -2,6 +2,12 @@
 
 cd ${TRAVIS_BUILD_DIR}
 
+# Grab the Mesos master logs
+mkdir -p ./mesos/master-logs
+mesos_master_container=$(docker ps --filter "name=minimesos-master-" --format "{{.ID}}")
+docker cp --follow-link $mesos_master_container:/var/log/mesos-master.INFO ./mesos/master-logs/
+docker cp --follow-link $mesos_master_container:/var/log/mesos-master.WARNING ./mesos/master-logs/
+
 tarball=./dump.txz
-tar -cJf $tarball --transform="s|\./[^/]*/\.*|${TRAVIS_JOB_NUMBER}/|" ./scheduler/log ./travis/.minimesos
+tar -cJf $tarball --transform="s|\./[^/]*/\.*|${TRAVIS_JOB_NUMBER}/|" ./scheduler/log ./travis/.minimesos ./mesos/master-logs
 ./travis/gdrive_upload "travis-${TRAVIS_JOB_NUMBER:-dump}" $tarball
