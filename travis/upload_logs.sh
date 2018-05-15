89cc6ada854447c6d1da8cd3fd7cbdde82c011e5
Daniel Posada <daniel.posada@gmail.com>
2018-05-15 14:55:35 -0500

    Uploads Mesos master logs even when container stopped (#850)

--- travis/upload_logs.sh
+++ travis/upload_logs.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd ${TRAVIS_BUILD_DIR}
 
 # Grab the Mesos master logs
 mkdir -p ./mesos/master-logs
-mesos_master_container=$(docker ps --filter "name=minimesos-master-" --format "{{.ID}}")
+docker ps --all --last 10
+mesos_master_container=$(docker ps --all --latest --filter "name=minimesos-master-" --format "{{.ID}}")
 docker cp --follow-link $mesos_master_container:/var/log/mesos-master.INFO ./mesos/master-logs/
 docker cp --follow-link $mesos_master_container:/var/log/mesos-master.WARNING ./mesos/master-logs/
 
