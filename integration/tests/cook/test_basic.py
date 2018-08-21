5a4f6421f4ac59408b1205cdd7acc7d5269c8dff
Daniel Posada <daniel.posada@gmail.com>
2018-08-21 15:14:00 -0500

    Un-marks test_balanced_host_constraint_cannot_place as xfail (#933)

--- integration/tests/cook/test_basic.py
+++ integration/tests/cook/test_basic.py
@@ -1679,12 +1679,7 @@ class CookTest(util.CookTest):
         finally:
             util.kill_jobs(self.cook_url, uuids)
 
-    @pytest.mark.xfail
     def test_balanced_host_constraint_cannot_place(self):
-        """
-        Marked as explicit due to broken constraints handling.
-        See GitHub issue #579.
-        """
         state = util.get_mesos_state(self.mesos_url)
         num_hosts = len(state['slaves'])
         if num_hosts > 10:
