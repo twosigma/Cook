37e88193c826b5c9eca89ef0a77e07c1498bc97d
Daniel Posada <daniel.posada@gmail.com>
2018-09-11 13:05:24 -0500

    Adds the new reason to reasons.py

--- integration/tests/cook/reasons.py
+++ integration/tests/cook/reasons.py
@@ -8,3 +8,4 @@ CMD_NON_ZERO_EXIT = 99003
 UNDER_INVESTIGATION = 'The job is now under investigation. Check back in a minute for more details!'
 COULD_NOT_PLACE_JOB = 'The job couldn\'t be placed on any available hosts.'
 JOB_WOULD_EXCEED_QUOTA = 'The job would cause you to exceed resource quotas.'
+JOB_IS_RUNNING_NOW = 'The job is running now.'
