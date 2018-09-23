bce3776338ba7ff2776aa181e5928634c387cbc0
Nick Vrvilo <nick.vrvilo@alumni.rice.edu>
2018-09-23 16:40:50 -0500

    Capture output from pytest when running integration tests (#975)

--- integration/travis/run_integration.sh
+++ integration/travis/run_integration.sh
@@ -160,9 +160,11 @@ export COOK_MULTI_CLUSTER=
 export COOK_MASTER_SLAVE=
 export COOK_SLAVE_URL=http://localhost:12323
 export COOK_MESOS_LEADER_URL=${MINIMESOS_MASTER}
-echo "Using Mesos leader URL: ${COOK_MESOS_LEADER_URL}"
-pytest -n4 -v --color=no --timeout-method=thread --boxed -m "not serial" || test_failures=true
-pytest -n0 -v --color=no --timeout-method=thread --boxed -m "serial" || test_failures=true
+{
+    echo "Using Mesos leader URL: ${COOK_MESOS_LEADER_URL}"
+    pytest -n4 -v --color=no --timeout-method=thread --boxed -m "not serial" || test_failures=true
+    pytest -n0 -v --color=no --timeout-method=thread --boxed -m "serial" || test_failures=true
+} &> >(tee ./log/pytest.log)
 
 # If there were failures, then we should save the logs
 if [ "$test_failures" = true ]; then
