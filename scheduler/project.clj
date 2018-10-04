f09e643c5dbaec782ed32baa648854e53aa85f10
Paul Schorfheide <paul.schorfheide@twosigma.com>
2018-10-04 12:34:30 -0500

    Snapshot version

--- scheduler/project.clj
+++ scheduler/project.clj
@@ -13,7 +13,7 @@
 ;; See the License for the specific language governing permissions and
 ;; limitations under the License.
 ;;
-(defproject cook "1.22.0"
+(defproject cook "1.22.1-SNAPSHOT"
   :description "This launches jobs on a Mesos cluster with fair sharing and preemption"
   :license {:name "Apache License, Version 2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
