29464a7c171cb87600a216cca51459c19e9234e9
Scott Crosby <scrosby@twosigma.com>
2018-10-09 14:23:19 -0500

    Start of hook unit test.

---
+++ scheduler/test/cook/test/hooks.clj
@@ -0,0 +1,22 @@
+(ns cook.hooks
+  (:require [clj-time.core :as t]))
+
+
+(def fake-scheduler-hooks
+  (reify SchedulerHooks
+    (check-job-submission-default [this] {:status :rejected :message "Too slow"})
+    (check-job-submission [this {:keys [name] :as job-map}]
+      (if (= name "reject-submission")
+        {:status :rejected :message "Explicit-reject"}
+        (default-accept)))
+    (check-job-invocation [this {:keys [name] :as job-map}]
+      (cond
+        ; job-a is accepted for 5 seconds, rejected for 5 seconds, valid for 5 seconds.
+        (and (= name "job-a") (t/before? (-> 5 t/seconds t/from-now)))
+            {:status :accepted :cache-expires-at (-> 5 t/seconds t/from-now)}
+            (and (= name "job-a") (t/before? (-> 10 t/seconds t/from-now)))
+            {:status :accepted :cache-expires-at (-> 5 t/seconds t/from-now)}
+
+
+
+                 (default-accept))))))
