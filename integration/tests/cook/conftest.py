80715c9a6a3c6d2fc974f7ca19a77ca34a958173
Daniel Posada <daniel.posada@twosigma.com>
2018-11-07 12:52:36 -0600

    Adds ssh check to conftest.py

--- integration/tests/cook/conftest.py
+++ integration/tests/cook/conftest.py
@@ -2,7 +2,9 @@
 # meaning it happens before any of the tests in this directory are run.
 # See the pytest documentation on conftest files for more information:
 # https://docs.pytest.org/en/2.7.3/plugins.html#conftest-py-plugins
+import logging
 import os
+import socket
 import subprocess
 import threading
 import time
@@ -25,9 +27,25 @@ def _sudo_checker_task(user):
         time.sleep(60)
 
 
-if util.kerberos_enabled() and \
-        os.getenv('COOK_MAX_TEST_USERS') and \
-        os.getenv('COOK_SWITCH_USER_MODE', 'sudo') == 'sudo':
-    username = next(util._test_user_names())
-    _sudo_check(username)
-    threading.Thread(target=_sudo_checker_task, args=[username], daemon=True).start()
+def _ssh_check(user):
+    """
+    Check if the current user can ssh as a test user.
+    This is necessary to obtain Kerberos auth headers for multi-user tests.
+    """
+    hostname = socket.gethostname()
+    logging.info(f'Checking ssh as {user} to {hostname}')
+    ssh_ok = (0 == subprocess.call(f'ssh {user}@{hostname} echo SSH', shell=True))
+    assert ssh_ok, f'Unable to ssh as {user} to {hostname}'
+
+
+if util.kerberos_enabled() and os.getenv('COOK_MAX_TEST_USERS'):
+    switch_user_mode = os.getenv('COOK_SWITCH_USER_MODE', 'sudo')
+    if switch_user_mode == 'sudo':
+        username = next(util._test_user_names())
+        _sudo_check(username)
+        threading.Thread(target=_sudo_checker_task, args=[username], daemon=True).start()
+    elif switch_user_mode == 'ssh':
+        for username in util._test_user_names():
+            _ssh_check(username)
+    else:
+        assert False, f'{switch_user_mode} is not a valid value for COOK_SWITCH_USER_MODE'
