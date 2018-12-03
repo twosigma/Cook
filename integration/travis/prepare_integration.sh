7b8f4e52a16063a0a8e9d018b8941b0367d6a4f3
Paul Schorfheide <paul.schorfheide@twosigma.com>
2018-12-03 13:46:54 -0600

    Explicitly uninstall CLI to fix caching issues

--- integration/travis/prepare_integration.sh
+++ integration/travis/prepare_integration.sh
@@ -4,4 +4,11 @@ set -ev
 
 PROJECT_DIR=`pwd` ../travis/prepare.sh
 python --version
+
+# Explicitly uninstall cli
+if [[ $(pip list --format=columns | grep cook-client) ]];
+then
+    pip uninstall -y cook-client
+fi
+
 pip install --user -r requirements.txt
