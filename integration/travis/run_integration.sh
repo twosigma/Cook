2f86e3a989f9d772853234f9d803b7d49eb2f458
Scott Crosby <scrosby@twosigma.com>
2018-12-10 13:47:36 -0600

    Remove rate limit tests form existing run_integration script.

--- integration/travis/run_integration.sh
+++ integration/travis/run_integration.sh
@@ -13,8 +13,6 @@ COOK_AUTH=one-user
 COOK_EXECUTOR=mesos
 COOK_POOLS=on
 CONFIG_FILE=scheduler_travis_config.edn
-JOB_LAUNCH_RATE_LIMIT=off
-GLOBAL_JOB_LAUNCH_RATE_LIMIT=off
 
 while (( $# > 0 )); do
   case "$1" in
@@ -28,14 +26,6 @@ while (( $# > 0 )); do
       ;;
     --pools=*)
       COOK_POOLS="${1#--pools=}"
-      shift
-      ;;
-    --job-launch-rate-limit=*)
-      JOB_LAUNCH_RATE_LIMIT="${1#--job-launch-rate-limit=}"
-      shift
-      ;;
-    --global-job-launch-rate-limit=*)
-      GLOBAL_JOB_LAUNCH_RATE_LIMIT="${1#--global-job-launch-rate-limit=}"
       shift
       ;;
     *)
@@ -117,42 +107,6 @@ case "$COOK_POOLS" in
     exit 1
 esac
 
-case "$JOB_LAUNCH_RATE_LIMIT" in
-    on)
-      # Note: Carefully chosen for test_rate_limit_launching_jobs unit test.
-      export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
-      export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
-      echo "Job launch rate limit turned on"
-    ;;
-    off)
-      # Note: Wide enough that we're unlikely to hit these in testing.
-      export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
-      export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
-      echo "Job launch rate limit turned off"
-    ;;
-  *)
-    echo "Unrecognized job-launch-rate-limit toggle (should be on/off): $JOB_LAUNCH_RATE_LIMIT"
-    exit 1
-esac
-
-case "$GLOBAL_JOB_LAUNCH_RATE_LIMIT" in
-    on)
-      # Note: Carefully chosen for test_global_rate_limit_launching_jobs unit test.
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
-      echo "Global job launch rate limit turned on"
-    ;;
-    off)
-      # Note: Wide enough that we're unlikely to hit these in testing.
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
-      echo "Global job launch rate limit turned off"
-    ;;
-  *)
-    echo "Unrecognized global-job-launch-rate-limit toggle (should be on/off): $GLOBAL_JOB_LAUNCH_RATE_LIMIT"
-    exit 1
-esac
-
 pip install flask
 export DATA_LOCAL_PORT=35847
 export DATA_LOCAL_SERVICE="http://localhost:${DATA_LOCAL_PORT}"
@@ -210,15 +164,8 @@ export COOK_SLAVE_URL=http://localhost:12323
 export COOK_MESOS_LEADER_URL=${MINIMESOS_MASTER}
 {
     echo "Using Mesos leader URL: ${COOK_MESOS_LEADER_URL}"
-    if [ "$JOB_LAUNCH_RATE_LIMIT" = on ]; then
-      pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_rate_limit_launching_jobs || test_failures=true
-    else if [ "$GLOBAL_JOB_LAUNCH_RATE_LIMIT" = on ]; then
-      pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_global_rate_limit_launching_jobs || test_failures=true
-    else
-      pytest -n4 -v --color=no --timeout-method=thread --boxed -m "not serial" || test_failures=true
-      pytest -n0 -v --color=no --timeout-method=thread --boxed -m "serial" || test_failures=true
-    fi
-    fi
+    pytest -n4 -v --color=no --timeout-method=thread --boxed -m "not serial" || test_failures=true
+    pytest -n0 -v --color=no --timeout-method=thread --boxed -m "serial" || test_failures=true
 } &> >(tee ./log/pytest.log)
  
 
