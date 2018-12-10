fd74b197c4c998099af69613aed5dfc5521d66c4
Scott Crosby <scrosby@twosigma.com>
2018-12-10 12:45:39 -0600

    Do both rate-limits sequentially

--- integration/travis/run_integration_ratelimit.sh
+++ integration/travis/run_integration_ratelimit.sh
@@ -26,9 +26,6 @@ while (( $# > 0 )); do
   esac
 done
 
-export COOK_HTTP_BASIC_AUTH=true
-COOK_EXECUTOR_COMMAND=""
-
 function wait_for_cook {
     COOK_PORT=${1:-12321}
     while ! curl -s localhost:${COOK_PORT} >/dev/null;
@@ -62,14 +59,25 @@ mkdir ${SCHEDULER_DIR}/log
 
 cd ${SCHEDULER_DIR}
 
+
+
 # Start two cook schedulers.
-# The basic tests will run against cook-framework-1
+export COOK_HTTP_BASIC_AUTH=true
+export COOK_EXECUTOR_COMMAND=""
+## We launch two instances, with different configurations for the different unit tests.
 ## on travis, ports on 172.17.0.1 are bindable from the host OS, and are also
 ## available for processes inside minimesos containers to connect to
-export COOK_EXECUTOR_COMMAND=${COOK_EXECUTOR_COMMAND}
 # Start one cook listening on port 12321, this will be the master of the "cook-framework-1" framework
+export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
+export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
+export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
+export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
 LIBPROCESS_IP=172.17.0.1 COOK_DATOMIC="${COOK_DATOMIC_URI_1}" COOK_PORT=12321 COOK_SSL_PORT=12322 COOK_COOKEEPER_LOCAL=true COOK_COOKEEPER_LOCAL_PORT=5291 COOK_FRAMEWORK_ID=cook-framework-1 COOK_LOGFILE="log/cook-12321.log" COOK_DEFAULT_POOL=${DEFAULT_POOL} lein run ${PROJECT_DIR}/travis/${CONFIG_FILE} &
 # Start a second cook listening on port 22321, this will be the master of the "cook-framework-2" framework
+export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
+export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
+export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
+export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
 LIBPROCESS_IP=172.17.0.1 COOK_DATOMIC="${COOK_DATOMIC_URI_1}" COOK_PORT=22321 COOK_SSL_PORT=22322 COOK_ZOOKEEPER_LOCAL=true COOK_ZOOKEEPER_LOCAL_PORT=4291 COOK_FRAMEWORK_ID=cook-framework-2 COOK_LOGFILE="log/cook-22321.log" lein run ${PROJECT_DIR}/travis/${CONFIG_FILE} &
 
 # Wait for the cooks to be listening
@@ -93,28 +101,15 @@ command -v cs
 # Run the integration tests
 cd ${PROJECT_DIR}
 export COOK_MESOS_LEADER_URL=${MINIMESOS_MASTER}
-
-export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
-export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
-export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10000
-export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=10000
-
-
 {
-    echo "Using Mesos leader URL: ${COOK_MESOS_LEADER_URL}"
-    if [ "$JOB_LAUNCH_RATE_LIMIT" = on ]; then
-      export COOK_SCHEDULER_URL=http://localhost:12321
-      export JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
-      export JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
-      pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_rate_limit_launching_jobs || test_failures=true
-    else if [ "$GLOBAL_JOB_LAUNCH_RATE_LIMIT" = on ]; then
-      export COOK_SCHEDULER_URL=http://localhost:22321
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_BUCKET_SIZE=10
-      export GLOBAL_JOB_LAUNCH_RATE_LIMIT_REPLENISHED_PER_MINUTE=5
-      pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_global_rate_limit_launching_jobs || test_failures=true
-    fi
-    fi
-} &> >(tee ./log/pytest.log)
+  echo "Using Mesos leader URL: ${COOK_MESOS_LEADER_URL}"
+  export COOK_SCHEDULER_URL=http://localhost:12321
+  pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_rate_limit_launching_jobs || test_failures=true
+
+
+  export COOK_SCHEDULER_URL=http://localhost:22321
+  pytest -n0 -v --color=no --timeout-method=thread --boxed -m multi_user tests/cook/test_multi_user.py -k test_global_rate_limit_launching_jobs || test_failures=true
+ } &> >(tee ./log/pytest.log)
  
 
 # If there were failures, then we should save the logs
