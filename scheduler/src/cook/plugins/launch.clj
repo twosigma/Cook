63cb2d6c38a9504f908fc6fada56dd076476e9c3
Scott Crosby <scott@sacrosby.com>
2019-03-04 12:40:54 -0600

    Expose accept-all-plugin (#1094)

--- scheduler/src/cook/plugins/launch.clj
+++ scheduler/src/cook/plugins/launch.clj
@@ -31,8 +31,9 @@
   "A default accept object with an expiration at infinity"
   {:status :accepted :message "No plugin object defined" :cache-expires-at cook.plugins.util/positive-infinity-date})
 
-(def accept-all-plugin
+(defn accept-all-plugin-factory
   "A plugin object that accepts everything. Available for use as a default plugin and unit testing."
+  []
   (reify JobLaunchFilter
     (check-job-launch [_ _] default-accept)))
 
@@ -51,7 +52,7 @@
             (log/info (str "Resolved as " resolved-fn))
             (resolved-fn))
           (throw (ex-info "Unable to resolve factory function" (assoc job-launch-filter :ns (namespace factory-fn))))))
-      accept-all-plugin)))
+      (accept-all-plugin-factory))))
 
 ;  Contains the plugin object that matches to a given job map. This code may create a new plugin object or re-use an existing one.
 (mount/defstate plugin-object
