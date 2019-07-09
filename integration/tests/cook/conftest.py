6b9106f73053a1b0fcab6ff3c8c857034ec9aa1f
Daniel Posada <daniel.posada@twosigma.com>
2019-07-09 17:34:08 -0500

    Adds metrics collection to integration tests

--- integration/tests/cook/conftest.py
+++ integration/tests/cook/conftest.py
@@ -2,12 +2,20 @@
 # meaning it happens before any of the tests in this directory are run.
 # See the pytest documentation on conftest files for more information:
 # https://docs.pytest.org/en/2.7.3/plugins.html#conftest-py-plugins
+import datetime
+import getpass
+import json
 import logging
 import os
 import socket
 import subprocess
 import threading
 import time
+import uuid
+
+import pytest
+from pygit2 import Repository
+
 from tests.cook import util
 
 
@@ -49,3 +57,40 @@ if util.kerberos_enabled() and os.getenv('COOK_MAX_TEST_USERS'):
             _ssh_check(username)
     else:
         assert False, f'{switch_user_mode} is not a valid value for COOK_SWITCH_USER_MODE'
+
+
+@pytest.fixture()
+def cleandir(request):
+    if 'TEST_METRICS_ES_URL' in os.environ:
+        print('\n~~~~~ hello from fixture')
+        yield
+        print('\n~~~~~ goodbye from fixture')
+        elastic_search_url = os.getenv('TEST_METRICS_ES_URL')
+        now = datetime.datetime.utcnow()
+        index = f'cook-tests-{now.strftime("%Y%m%d")}'
+        test_namespace = '::'.join(request.node._nodeid.split('::')[:-1]).replace('/', '.')
+        test_name = request.node.name
+        doc_id = f'{test_namespace}-{test_name}-{now.strftime("%s")}-{uuid.uuid4()}'
+        repository_path = os.path.abspath(f'{os.path.dirname(os.path.abspath(__file__))}/../../..')
+        repo = Repository(repository_path)
+        head = repo.head
+        commit = repo.revparse_single('HEAD')
+        metrics = {
+            'timestamp': now.strftime('%Y-%m-%dT%H:%M:%S'),
+            'project': 'cook',
+            'test-namespace': test_namespace,
+            'test-name': test_name,
+            'git-branch': head.name.replace('refs/heads/', ''),
+            'git-commit-hash': commit.hex,
+            'git-branch-under-test': os.getenv('TEST_METRICS_BRANCH_UNDER_TEST', None),
+            'git-commit-hash-under-test': os.getenv('TEST_METRICS_COMMIT_HASH_UNDER_TEST', None),
+            'host': socket.gethostname(),
+            'user': getpass.getuser(),
+            'build-id': os.getenv('TEST_METRICS_BUILD_ID', None),
+            'result': 
+        }
+        logging.info(f'Updating test metrics: {metrics}')
+        resp = util.session.post(f'{elastic_search_url}/{index}/test/{doc_id}', json=metrics)
+        logging.info(f'Response from updating test metrics: {json.dumps(resp.json(), indent=2)}')
+    else:
+        yield
