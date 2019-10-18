ad83346ad99d18df851a77426eac5f7d4c8cc7af
Scott Crosby <scrosby@twosigma.com>
2019-10-18 12:04:35 -0500

    Move some other handlers from a compute-cluster/kill to compute-cluster/kill-task-if-possible.
    There are cases, in particular, lingering tasks, stragglers, or cancelled tasks where the task might
    outlive the compute cluster it was member of. When this occurs, the looked up compute cluster
    is nil and trying to kill via it would cause an NPE. Switch them to using this wrapper function.

--- scheduler/src/cook/compute_cluster.clj
+++ scheduler/src/cook/compute_cluster.clj
@@ -48,7 +48,10 @@
     "Called when offers are not processed to ensure they're still available."))
 
 (defn kill-task-if-possible [compute-cluster task-id]
-  "If compute cluster is nil, print a warning instead of killing the task"
+  "If compute cluster is nil, print a warning instead of killing the task. There are cases, in particular,
+  lingering tasks, stragglers, or cancelled tasks where the task might outlive the compute cluster it was
+  member of. When this occurs, the looked up compute cluster is null and trying to kill via it would cause an NPE,
+  when in really, its relatively innocuous. So, we have this wrapper to use in those circumstances."
   (if compute-cluster
     (kill-task compute-cluster task-id)
     (log/warn "Unable to kill task " task-id " because compute-cluster is nil")))
