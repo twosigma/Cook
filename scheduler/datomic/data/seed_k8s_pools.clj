fae8b9d6d645259f39966236893b135eb46c7377
Scott Crosby <scrosby@twosigma.com>
2019-11-08 14:03:44 -0600

    Added seed_k8s_pools command for generating k8s pools.

---
+++ scheduler/datomic/data/seed_k8s_pools.clj
@@ -0,0 +1,45 @@
+(ns data.seed-k8s-pools
+  (:require [cook.datomic :as datomic]
+            [cook.quota :as quota]
+            [datomic.api :as d]))
+
+(def uri (second *command-line-args*))
+(println "Datomic URI is" uri)
+
+(defn create-pool
+  [conn name state]
+  (println "Creating pool" name)
+  @(d/transact conn [{:db/id (d/tempid :db.part/user)
+                      :pool/name name
+                      :pool/purpose "This is a pool for testing purposes"
+                      :pool/state state
+                      :pool/dru-mode :pool.dru-mode/default}]))
+
+(defn pools
+  [db]
+  (->> (d/q '[:find [?p ...]
+              :in $ [?state ...]
+              :where
+              [?p :pool/state ?state]]
+            db [:pool.state/active :pool.state/inactive])
+       (map (partial d/entity db))
+       (map d/touch)))
+
+(try
+  (let [conn (datomic/create-connection {:settings {:mesos-datomic-uri uri}})]
+    (println "Connected to Datomic:" conn)
+    (create-pool conn "k8s-alpha" :pool.state/active)
+    (create-pool conn "k8s-beta" :pool.state/inactive)
+    (create-pool conn "k8s-gamma" :pool.state/active)
+    (create-pool conn "k8s-delta" :pool.state/inactive)
+    (quota/set-quota! conn "default" "k8s-alpha" "For quota-related testing." :cpus 8 :mem 1024)
+    (quota/set-quota! conn "default" "k8s-gamma" "For quota-related testing." :cpus 9 :mem 2048)
+    (println "Pools & Quotas:")
+    (run! (fn [{:keys [pool/name] :as p}]
+            (clojure.pprint/pprint p)
+            (clojure.pprint/pprint (quota/get-quota (d/db conn) "default" name)))
+          (pools (d/db conn)))
+    (System/exit 0))
+  (catch Throwable t
+    (println "Failed to seed pools:" t)
+    (System/exit 1)))
