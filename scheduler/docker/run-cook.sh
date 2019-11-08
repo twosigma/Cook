d141794702da38057b218142dba0e5d91911215f
Scott Crosby <scrosby@twosigma.com>
2019-11-08 14:06:50 -0600

    Change how docker runs cook.

--- scheduler/docker/run-cook.sh
+++ scheduler/docker/run-cook.sh
@@ -6,5 +6,6 @@ echo "alt-host=$(hostname -i | cut -d' ' -f2)" >> ${DATOMIC_PROPERTIES_FILE}
 /opt/cook/datomic-free-0.9.5394/bin/transactor ${DATOMIC_PROPERTIES_FILE} &
 echo "Seeding test data..."
 lein exec -p /opt/cook/datomic/data/seed_pools.clj ${COOK_DATOMIC_URI}
+lein exec -p /opt/cook/datomic/data/seed_k8s_pools.clj ${COOK_DATOMIC_URI}
 lein exec -p /opt/cook/datomic/data/seed_running_jobs.clj ${COOK_DATOMIC_URI}
 lein with-profiles +docker run $1
