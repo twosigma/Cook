ab6ae1ffd76b0f90a6b6a725188b62588ecea649
Daniel Posada <daniel.posada@gmail.com>
2019-12-12 11:06:47 -0600

    Adds mapping for Failed pod phase (#1314)
    * Adds mapping for Failed pod phase
    
    * Adds unit tests
    
    * Adds log when encountering OutOfcpu
    
    * Removes :ancillary from state map and improves logging
    
    * Fixes unit tests

--- scheduler/test/cook/test/kubernetes/controller.clj
+++ scheduler/test/cook/test/kubernetes/controller.clj
@@ -1,14 +1,12 @@
 (ns cook.test.kubernetes.controller
   (:require [clojure.test :refer :all]
-            [cook.kubernetes.api :as controller]
-            [cook.test.testutil :as tu]
-            [datomic.api :as d])
-  (:import (io.kubernetes.client.models V1Container V1EnvVar V1Pod V1PodStatus V1ContainerStatus V1ContainerState V1ContainerStateWaiting)))
+            [cook.kubernetes.controller :as controller])
+  (:import (io.kubernetes.client.models V1ObjectMeta V1Pod V1PodStatus)))
 
 
-(deftest existing-state-equivalent?
-  ;; TODO
-  )
+(deftest test-existing-state-equivalent?
+  (testing "different states"
+    (is (not (controller/existing-state-equivalent? {:state :pod/failed} {:state :pod/succeeded})))))
 
 (deftest expected-state-equivalent?
   ;; TODO
@@ -18,9 +16,14 @@
   ;; TODO
   )
 
-(deftest container-status->failure-reason
-  ;; TODO
-  )
+(deftest test-container-status->failure-reason
+  (testing "no container status + out-of-cpu pod status"
+    (let [pod-status (V1PodStatus.)
+          container-status nil]
+      (.setReason pod-status "OutOfcpu")
+      (is (= :reason-invalid-offers
+             (controller/container-status->failure-reason {:name "test-cluster"} "12345"
+                                                          pod-status container-status))))))
 
 (deftest process-successful
   ;; TODO
@@ -62,3 +65,15 @@
 (deftest update-expected-state
   ;; TODO
   )
+
+(deftest test-pod-has-just-completed
+  (testing "graceful handling of lack of exit code"
+    (let [pod (V1Pod.)
+          pod-metadata (V1ObjectMeta.)
+          pod-status (V1PodStatus.)]
+      (.setMetadata pod pod-metadata)
+      (.setReason pod-status "OutOfcpu")
+      (.setStatus pod pod-status)
+      (with-redefs [controller/write-status-to-datomic (constantly nil)]
+        (is (= {:expected-state :expected/completed}
+               (controller/pod-has-just-completed nil {:pod pod :synthesized-state {:state :pod/failed}})))))))
