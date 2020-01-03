98608a2b66136d0d751f7f14c68a1457d9c16e4b
Daniel Posada <daniel.posada@gmail.com>
2020-01-03 13:10:59 -0600

    Addresses code review feedback

--- integration/tests/cook/util.py
+++ integration/tests/cook/util.py
@@ -1716,3 +1716,31 @@ def job_progress_is_present(job, progress):
     if not present:
         logger.info(f'Job does not yet have progress {progress}: {json.dumps(job, indent=2)}')
     return present
+
+
+def make_failed_job(cook_url, **kwargs):
+    def __make_failed_job():
+        job_uuid, resp = submit_job(cook_url, **kwargs)
+        assert resp.status_code == 201
+
+        # Wait for the job to start running, and kill it
+        # It's possible for our kill below to race with
+        # e.g. an "Agent removed" failure, which is why
+        # we check for a non-mea-culpa failure at the end
+        wait_for_job(cook_url, job_uuid, 'running')
+        kill_jobs(cook_url, [job_uuid])
+
+        def instance_query():
+            return query_jobs(cook_url, True, uuid=[job_uuid])
+
+        # Wait for the job (and its instances) to die
+        wait_until(instance_query, all_instances_killed)
+        job = load_job(cook_url, job_uuid)
+        assert 'failed' == job['state']
+
+        # Ensure that there is a non-mea culpa failure
+        non_mea_culpa_failure_exists = any(not i['reason_mea_culpa'] for i in job['instances'])
+        assert non_mea_culpa_failure_exists
+        return job
+
+    return wait_until(__make_failed_job, lambda _: True)
