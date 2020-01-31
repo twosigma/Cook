b2aa10d6b79e13238dd09689199a439d6761bcca
Daniel Posada <daniel.posada@gmail.com>
2020-01-30 20:21:50 -0600

    Update scheduler/src/cook/kubernetes/api.clj
    Co-Authored-By: Shams <shamsimam@users.noreply.github.com>

--- scheduler/src/cook/kubernetes/api.clj
+++ scheduler/src/cook/kubernetes/api.clj
@@ -690,7 +690,7 @@
         true
         (catch ApiException e
           (let [code (.getCode e)
-                bad-pod-spec? (some #{code} [404 422])]
+                bad-pod-spec? (contains? #{404 422} code)]
             (log/info e "Error submitting pod with name" pod-name "in namespace" namespace
                       ", code:" code ", response body:" (.getResponseBody e))
             (not bad-pod-spec?)))))
