6fc2072365f52b65fe6e9a2dbef5f77fe9ac89f7
Scott Crosby <scott@sacrosby.com>
2020-02-04 11:30:11 -0600

    Add support to blacklist nodes in kubernetes that have certain… (#1399)

--- scheduler/src/cook/kubernetes/api.clj
+++ scheduler/src/cook/kubernetes/api.clj
@@ -241,22 +241,29 @@
     (-> node-name->pods (get node-name []) count)))
 
 (defn node-schedulable?
-  "Can we schedule on a node. For now, yes, unless there are other taints on it. TODO: Incorporate other node-health measures here."
-  [^V1Node node pod-count-capacity pods]
+  "Can we schedule on a node. For now, yes, unless there are other taints on it or it contains any label in the
+  node-blocklist-labels list.
+  TODO: Incorporate other node-health measures here."
+  [^V1Node node pod-count-capacity pods node-blocklist-labels]
   (if (nil? node)
     false
     (let [taints-on-node (or (some-> node .getSpec .getTaints) [])
           other-taints (remove #(= "cook-pool" (.getKey %)) taints-on-node)
-          schedulable (zero? (count other-taints))
           node-name (some-> node .getMetadata .getName)
           pods-on-node (num-pods-on-node node-name pods)
-          below-pod-count-capacity (< pods-on-node pod-count-capacity)]
-      (when-not schedulable
-        (log/info "Filtering out" node-name "because it has taints" other-taints))
-      (when-not below-pod-count-capacity
-        (log/info "Filtering out" node-name "because it is at or above its pod count capacity of"
-                  pod-count-capacity "(" pods-on-node ")"))
-      (and schedulable below-pod-count-capacity))))
+          labels-on-node (or (some-> node .getMetadata .getLabels) {})
+          matching-node-blocklist-labels (some #(get labels-on-node %) node-blocklist-labels)]
+      (cond  (seq other-taints) (do
+                                  (log/info "Filtering out" node-name "because it has taints" other-taints)
+                                  false)
+             (>= pods-on-node pod-count-capacity) (do
+                                                    (log/info "Filtering out" node-name "because it is at or above its pod count capacity of"
+                                                              pod-count-capacity "(" pods-on-node ")")
+                                                    false)
+             (seq matching-node-blocklist-labels) (do
+                                                    (log/info "Filtering out" node-name "because it has node blocklist labels" matching-node-blocklist-labels)
+                                                    false)
+             :else true))))
 
 (defn get-capacity
   "Given a map from node-name to node, generate a map from node-name->resource-type-><capacity>"
