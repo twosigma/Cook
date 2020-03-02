84f39938da007222bf6a89961af8633fb644c476
Daniel Posada <daniel.posada@gmail.com>
2020-03-02 11:19:52 -0600

    Sets the failure limit to 10 for container launch failed

--- scheduler/src/cook/schema.clj
+++ scheduler/src/cook/schema.clj
@@ -1326,7 +1326,10 @@ for a job. E.g. {:resources {:cpus 4 :mem 3} :constraints {\"unique_host_constra
     :reason/string "Container launch failed"
     :reason/name :mesos-container-launch-failed
     :reason/mea-culpa? true
-    :reason/mesos-reason :reason-container-launch-failed}
+    :reason/mesos-reason :reason-container-launch-failed
+    ;; Unless configured otherwise, start counting more
+    ;; than 10 failures against the job's retry limit
+    :reason/failure-limit 10}
    {:db/id (d/tempid :db.part/user)
     :reason/code 4004
     :reason/string "Container update failed"
