7d513c6ff55a6ed80cb6afe277f427c9b70cc4ec
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2020-06-02 09:59:01 -0400

    Re-introduce optionals

--- jobclient/python/cook/scheduler/client/jobs.py
+++ jobclient/python/cook/scheduler/client/jobs.py
@@ -16,7 +16,7 @@ from copy import deepcopy
 from datetime import datetime, timedelta
 from enum import Enum
 from uuid import UUID
-from typing import Dict, List, Set
+from typing import Dict, List, Optional, Set
 
 from . import constraints
 from .constraints import Constraint
@@ -111,27 +111,27 @@ class Job:
     status: Status
     state: State
     priority: int
-    disable_mea_culpa_retries: bool
-    executor: Executor
-    expected_runtime: timedelta
-    pool: str
-    instances: List[Instance]
-    env: Dict[str, str]
-    uris: List[FetchableUri]
-    container: dict
-    labels: Dict[str, str]
-    constraints: Set[Constraint]
-    groups: List[UUID]
-    application: Application
-    progress_output_file: str
-    progress_regex_string: str
-    user: str
-    datasets: list
-    gpus: int
     framework_id: str
-    ports: int
-    submit_time: datetime
     retries_remaining: int
+    submit_time: datetime
+    user: str
+    executor: Optional[Executor]
+    container: Optional[dict]
+    disable_mea_culpa_retries: Optional[bool]
+    expected_runtime: Optional[timedelta]
+    pool: Optional[str]
+    instances: Optional[List[Instance]]
+    env: Optional[Dict[str, str]]
+    uris: Optional[List[FetchableUri]]
+    labels: Optional[Dict[str, str]]
+    constraints: Optional[Set[Constraint]]
+    group: Optional[UUID]
+    application: Optional[Application]
+    progress_output_file: Optional[str]
+    progress_regex_string: Optional[str]
+    datasets: Optional[dict]
+    gpus: Optional[int]
+    ports: Optional[int]
 
     def __init__(self, *,
                  # Required args
@@ -142,36 +142,37 @@ class Job:
                  name: str,
                  max_retries: int,
                  max_runtime: timedelta,
-                 state: State,
                  status: Status,
+                 state: State,
                  priority: int,
-                 disable_mea_culpa_retries: bool,
-                 executor: Executor,
-                 expected_runtime: int,
-                 pool: str,
-                 instances: List[Instance],
-                 env: Dict[str, str],
-                 uris: List[FetchableUri],
-                 container: dict,
-                 labels: Dict[str, str],
-                 constraints: Set[Constraint],
-                 groups: List[UUID],
-                 application: Application,
-                 progress_output_file: str,
-                 progress_regex_string: str,
-                 user: str,
-                 datasets: list,
-                 gpus: float,
                  framework_id: str,
-                 ports: int,
-                 submit_time: int,
-                 retries_remaining: int):
+                 retries_remaining: int,
+                 submit_time: datetime,
+                 user: str,
+                 # Optional keys
+                 executor: Optional[Executor] = None,
+                 container: Optional[dict] = None,
+                 disable_mea_culpa_retries: Optional[bool] = None,
+                 expected_runtime: Optional[timedelta] = None,
+                 pool: Optional[str] = None,
+                 instances: Optional[List[Instance]] = None,
+                 env: Optional[Dict[str, str]] = None,
+                 uris: Optional[List[FetchableUri]] = None,
+                 labels: Optional[Dict[str, str]] = None,
+                 constraints: Optional[Set[Constraint]] = None,
+                 group: Optional[UUID] = None,
+                 application: Optional[Application] = None,
+                 progress_output_file: Optional[str] = None,
+                 progress_regex_string: Optional[str] = None,
+                 datasets: Optional[dict] = None,
+                 gpus: Optional[int] = None,
+                 ports: Optional[int] = None):
         """Initializes a job object.
 
         Normally, this function wouldn't be invoked directly. It is instead
         used by `JobClient` when getting the data from `query`.
 
-        Parameters
+        Required Parameters
         ----------
         :param command: The command-line string that this job will execute.
         :type command: str
@@ -185,12 +186,23 @@ class Job:
         :type max_retries: int
         :param max_runtime: The maximum time that this job may run.
         :type max_runtime: timedelta
-        :param state: The current state of the job.
-        :type state: State
         :param status: The current status of the job.
         :type status: Status
+        :param state: The current state of the job.
+        :type state: State
         :param priority: The current priority of the job, defaults to 50
         :type priority: int
+        :param framework_id: Framework ID for this job.
+        :type framework_id: str
+        :param retries_remaining: The number of retries remaining for this job.
+        :type retries_remaining: int
+        :param submit_time: The point in time when this job was submitted.
+        :type submit_time: datetime
+        :param user: User running this job.
+        :type user: str
+
+        Optional Parameters
+        -------------------
         :param disable_mea_culpa_retries: If true, then the job will not issue
             *mea culpa* retries.
         :type disable_mea_culpa_retries: bool
@@ -220,20 +232,12 @@ class Job:
         :type progress_output_file: str
         :param progress_regex_string: Progress regex.
         :type progress_regex_string: str
-        :param user: User running this job.
-        :type user: str
         :param datasets: Datasets associated with this job.
         :type datasets: list
         :param gpus: The number of GPUs to request from Cook.
         :type gpus: int
-        :param framework_id: Framework ID for this job.
-        :type framework_id: str
         :param ports: Ports for this job.
         :type ports: int
-        :param submit_time: The point in time when this job was submitted.
-        :type submit_time: datetime
-        :param retries_remaining: The number of retries remaining for this job.
-        :type retries_remaining: int
         """
         self.command = command
         self.mem = mem
@@ -242,34 +246,34 @@ class Job:
         self.name = name
         self.max_retries = max_retries
         self.max_runtime = max_runtime
-        self.state = state
         self.status = status
+        self.state = state
         self.priority = priority
-        self.disable_mea_culpa_retries = disable_mea_culpa_retries
+        self.framework_id = framework_id
+        self.retries_remaining = retries_remaining
+        self.submit_time = submit_time
+        self.user = user
         self.executor = executor
+        self.container = container
+        self.disable_mea_culpa_retries = disable_mea_culpa_retries
         self.expected_runtime = expected_runtime
         self.pool = pool
         self.instances = instances
         self.env = env
         self.uris = uris
-        self.container = container
         self.labels = labels
         self.constraints = constraints
-        self.groups = groups
+        self.group = group
         self.application = application
         self.progress_output_file = progress_output_file
         self.progress_regex_string = progress_regex_string
-        self.user = user
         self.datasets = datasets
         self.gpus = gpus
-        self.framework_id = framework_id
         self.ports = ports
-        self.submit_time = submit_time
-        self.retries_remaining = retries_remaining
 
     def to_dict(self) -> dict:
         """Generate this job's `dict` representation."""
-        return {
+        d = {
             'command': self.command,
             'mem': self.mem,
             'cpus': self.cpus,
@@ -280,42 +284,75 @@ class Job:
             'status': str(self.status),
             'state': str(self.state),
             'priority': self.priority,
-            'disable_mea_culpa_retries': self.disable_mea_culpa_retries,
-            'executor': str(self.executor),
-            'expected_runtime': int(self.expected_runtime.total_seconds()),
-            'pool': self.pool,
-            'instances': list(map(str, self.instances)),
-            'env': self.env,
-            'uris': list(map(FetchableUri.to_dict, self.uris)),
-            'container': self.container,
-            'labels': self.labels,
-            'constraints': list(map(lambda c: c.to_list, self.constraints)),
-            'groups': list(map(str, self.groups)),
-            'application': self.application.to_dict(),
-            'progress_output_file': self.progress_output_file,
-            'progress_regex_string': self.progress_regex_string,
-            'user': self.user,
-            'datasets': self.datasets,
-            'gpus': self.gpus,
             'framework_id': self.framework_id,
-            'ports': self.ports,
-            'submit_time': datetime.fromtimestamp(self.submit_time),
             'retries_remaining': self.retries_remaining,
+            'submit_time': int(self.submit_time.timestamp()),
+            'user': self.user
         }
+        if self.disable_mea_culpa_retries is not None:
+            d['disable_mea_culpa_retries'] = self.disable_mea_culpa_retries
+        if self.executor is not None:
+            d['executor'] = str(self.executor)
+        if self.expected_runtime is not None:
+            d['expected_runtime'] = int(self.expected_runtime.total_seconds())
+        if self.pool is not None:
+            d['pool'] = self.pool
+        if self.instances is not None:
+            d['instances'] = list(map(str, self.instances))
+        if self.env is not None:
+            d['env'] = self.env
+        if self.uris is not None:
+            d['uris'] = list(map(FetchableUri.to_dict, self.uris))
+        if self.container is not None:
+            d['container'] = self.container
+        if self.labels is not None:
+            d['labels'] = self.labels
+        if self.constraints is not None:
+            d['constraints'] = list(map(lambda c: c.to_list, self.constraints))
+        if self.groups is not None:
+            d['groups'] = list(map(str, self.groups))
+        if self.application is not None:
+            d['application'] = self.application.to_dict()
+        if self.progress_output_file is not None:
+            d['progress_output_file'] = self.progress_output_file
+        if self.progress_regex_string is not None:
+            d['progress_regex_string'] = self.progress_regex_string
+        if self.user is not None:
+            d['user'] = self.user
+        if self.datasets is not None:
+            d['datasets'] = self.datasets
+        if self.gpus is not None:
+            d['gpus'] = self.gpus
+        if self.framework_id is not None:
+            d['framework_id'] = self.framework_id
+        if self.ports is not None:
+            d['ports'] = self.ports
+        if self.submit_time is not None:
+            d['submit_time'] = datetime.fromtimestamp(self.submit_time)
+        if self.retries_remaining is not None:
+            d['retries_remaining'] = self.retries_remaining
+        return d
 
     @classmethod
     def from_dict(cls, d: dict) -> 'Job':
         """Parse a Job from its `dict` representation."""
         d = deepcopy(d)
         d['uuid'] = UUID(d['uuid'])
+        d['max_runtime'] = timedelta(seconds=d['max_runtime'])
         d['status'] = Status.from_string(d['status'])
         d['state'] = State.from_string(d['state'])
-        d['max_runtime'] = timedelta(seconds=d['max_runtime'])
-        d['application'] = Application.from_dict(d['application'])
-        d['uris'] = list(map(FetchableUri.from_dict, d['uris']))
-        d['constraints'] = set(map(constraints.parse_from, d['constraints']))
-        d['executor'] = Executor.from_string(d['executor'])
-        d['instances'] = list(map(Instance.from_dict, d['instances']))
-        d['expected_runtime'] = timedelta(seconds=d['expected_runtime'])
         d['submit_time'] = datetime.fromtimestamp(d['submit_time'])
+
+        if 'executor' in d:
+            d['executor'] = Executor.from_string(d['executor'])
+        if 'expected_runtime' in d:
+            d['expected_runtime'] = timedelta(seconds=d['expected_runtime'])
+        if 'instances' in d:
+            d['instances'] = list(map(Instance.from_dict, d['instances']))
+        if 'uris' in d:
+            d['uris'] = list(map(FetchableUri.from_dict, d['uris']))
+        if 'constraints' in d:
+            d['constraints'] = set(map(constraints.parse_from, d['constraints']))  # noqa E501
+        if 'application' in d:
+            d['application'] = Application.from_dict(d['application'])
         return cls(**d)
