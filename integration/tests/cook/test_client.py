a9664844e5475546f61412fa7380200a192a02cd
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2020-06-09 17:55:32 -0400

    Fix setUpClass typo

--- integration/tests/cook/test_client.py
+++ integration/tests/cook/test_client.py
@@ -20,7 +20,7 @@ from tests.cook import util
 
 class ClientTest(util.CookTest):
     @classmethod
-    def setUpCls(cls):
+    def setUpClass(cls):
         cls.cook_url = util.retreive_cook_url()
 
     def setUp(self):
