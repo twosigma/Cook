8ca1bb8b2df15d85fc7fdd62835240a9731bbe0b
Héctor Rodríguez <hdrodz97@users.noreply.github.com>
2020-06-18 12:48:47 -0400

    Missing import

--- integration/tests/cook/test_client.py
+++ integration/tests/cook/test_client.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
+
 from cookclient import JobClient
 from cookclient.containers import DockerContainer
 from cookclient.jobs import Job, Status as JobStatus
