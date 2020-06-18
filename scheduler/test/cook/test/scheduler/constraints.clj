b4f7f2e24290fddd80e885d3d13feecbc4065d05
[Kathryn <kathrynzhou@kathryns-mbp-2.lan>
2020-06-18 12:45:30 -0400

    updated constraints test

--- scheduler/test/cook/test/scheduler/constraints.clj
+++ scheduler/test/cook/test/scheduler/constraints.clj
@@ -95,20 +95,7 @@
                                 (getHostname [_] "test-host")
                                 (getRunningTasks [_] [])
                                 (getTasksCurrentlyAssigned [_] [])
-                                (getCurrAvailableResources [_]  (sched/->VirtualMachineLeaseAdapter gpu-offer 0)))]
-                              ["running gpu"
-                               (reify com.netflix.fenzo.VirtualMachineCurrentState
-                                (getHostname [_] "test-host")
-                                (getRunningTasks [_] [(sched/make-task-request db other-gpu-job nil)])
-                                (getTasksCurrentlyAssigned [_] [])
-                                (getCurrAvailableResources [_]  (sched/->VirtualMachineLeaseAdapter non-gpu-offer 0)))]
-                              ["gpu assigned"
-                               (reify com.netflix.fenzo.VirtualMachineCurrentState
-                                (getHostname [_] "test-host")
-                                (getRunningTasks [_] [])
-                                (getTasksCurrentlyAssigned [_] [(mock-gpu-assignment)])
-                                (getCurrAvailableResources [_]  (sched/->VirtualMachineLeaseAdapter non-gpu-offer 0)))]
-                              ]]
+                                (getCurrAvailableResources [_]  (sched/->VirtualMachineLeaseAdapter gpu-offer 0)))]]]
       (is (.isSuccessful
             (.evaluate (constraints/fenzoize-job-constraint (constraints/build-gpu-host-constraint gpu-job))
                        (sched/make-task-request db gpu-job nil)
