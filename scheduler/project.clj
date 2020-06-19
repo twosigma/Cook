cce4b6e65ee285027b64197b78bb5bc3f1adf50c
Daniel Posada <daniel.posada@gmail.com>
2020-06-18 21:55:02 -0500

    Bumps Java jobclient version to 0.6.1-SNAPSHOT

--- scheduler/project.clj
+++ scheduler/project.clj
@@ -191,7 +191,7 @@
    {:dependencies [[criterium "0.4.4"]
                    [org.clojure/test.check "0.6.1"]
                    [org.mockito/mockito-core "1.10.19"]
-                   [twosigma/cook-jobclient "0.5.1-SNAPSHOT"]]}
+                   [twosigma/cook-jobclient "0.6.1-SNAPSHOT"]]}
 
    :test-console
    [:test {:jvm-opts ["-Dcook.test.logging.console"]}]
