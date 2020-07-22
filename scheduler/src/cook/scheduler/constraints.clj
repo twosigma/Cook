fa8c5dc10a0bf653eb3390f4a59735d979373760
Kathryn Zhou <52860192+kathryn-zhou@users.noreply.github.com>
2020-07-21 23:27:20 -0400

    Update scheduler/src/cook/scheduler/constraints.clj
    Co-authored-by: Daniel Posada <daniel.posada@gmail.com>

--- scheduler/src/cook/scheduler/constraints.clj
+++ scheduler/src/cook/scheduler/constraints.clj
@@ -114,7 +114,7 @@
                                             job-gpu-count-requested job (util/job->pool-name job))
                   vm-gpu-model->count-available (get vm-attributes "gpus")
                   vm-satisfies-constraint? (if (pos? job-gpu-count-requested)
-                                             ; If job requests GPUs, require that the VM has enough gpus available in the same model as the job requested.
+                                             ; If job requests GPUs, require that the VM has the same number of gpus available in the same model as the job requested.
                                              (== (get vm-gpu-model->count-available job-gpu-model-requested 0) job-gpu-count-requested)
                                              ; If job does not request GPUs, require that the VM does not support gpus.
                                              (-> vm-gpu-model->count-available count zero?))]
