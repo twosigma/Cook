7544141313df41ea7a1f7217b547b9bd2a353f99
Héctor D. Rodríguez Figueroa <hdrodz97@users.noreply.github.com>
2020-07-23 10:13:12 -0400

    Fix Python job client docs for readthedocs (#1615)

--- jobclient/python/docs/source/conf.py
+++ jobclient/python/docs/source/conf.py
@@ -57,3 +57,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Set the master_doc value, as readthedocs uses an older version of Sphinx
+# which will default to `contents` instead of `index`.
+master_doc = 'index'
