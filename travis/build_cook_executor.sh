40817c42b28b80ed2d2c68844918a2100d7fd2aa
tingtongtang <kdt0116@tamu.edu>
2020-11-28 11:50:57 -0600

    get pip version and run without wrapper

--- travis/build_cook_executor.sh
+++ travis/build_cook_executor.sh
@@ -3,6 +3,7 @@
 set -ev
 
 cd ${GITHUB_WORKSPACE}/executor
-/opt/hostedtoolcache/Python/3.6.12/x64/bin/pip install --user -r requirements.txt
+python -m pip --version
+python -m pip install --user -r requirements.txt
 ./bin/prepare-executor.sh local ${GITHUB_WORKSPACE}/scheduler/resources/public
 tar -C ${GITHUB_WORKSPACE}/travis -xzf ./dist/cook-executor-local.tar.gz
