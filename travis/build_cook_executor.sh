2ee4e87841d9e4d4259a2fa827de3b3653ba6922
tingtongtang <kdt0116@tamu.edu>
2020-11-28 11:44:22 -0600

    debugging with absolute path

--- travis/build_cook_executor.sh
+++ travis/build_cook_executor.sh
@@ -3,6 +3,6 @@
 set -ev
 
 cd ${GITHUB_WORKSPACE}/executor
-pip install --user -r requirements.txt
+/opt/hostedtoolcache/Python/3.6.12/x64/bin/pip install --user -r requirements.txt
 ./bin/prepare-executor.sh local ${GITHUB_WORKSPACE}/scheduler/resources/public
 tar -C ${GITHUB_WORKSPACE}/travis -xzf ./dist/cook-executor-local.tar.gz
