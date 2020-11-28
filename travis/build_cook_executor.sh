0bba6fc29e953195c56d7c8428880b271e3f9cef
tingtongtang <kdt0116@tamu.edu>
2020-11-28 12:05:04 -0600

    export env varible in same step

--- travis/build_cook_executor.sh
+++ travis/build_cook_executor.sh
@@ -3,7 +3,6 @@
 set -ev
 
 cd ${GITHUB_WORKSPACE}/executor
-python -m pip --version
-python -m pip install --user -r requirements.txt
+pip install --user -r requirements.txt
 ./bin/prepare-executor.sh local ${GITHUB_WORKSPACE}/scheduler/resources/public
 tar -C ${GITHUB_WORKSPACE}/travis -xzf ./dist/cook-executor-local.tar.gz
