d2829dbeb20ccccd78ac5b01417192787d711d8b
Daniel Posada <daniel.posada@gmail.com>
2020-12-03 14:34:49 -0600

    Bumps python jobclient to v0.4.0

--- jobclient/python/cookclient/__init__.py
+++ jobclient/python/cookclient/__init__.py
@@ -28,7 +28,7 @@ from . import util
 from .containers import AbstractContainer
 from .jobs import Application, Disk, Job
 
-CLIENT_VERSION = '0.3.1'
+CLIENT_VERSION = '0.4.0'
 
 _LOG = logging.getLogger(__name__)
 _LOG.addHandler(logging.StreamHandler())
