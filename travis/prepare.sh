7fc717ded7710f03f3bb474a0631d7b90a9a38b9
Kevin Tang <kevin.tang@twosigma.com>
2020-12-04 09:21:40 -0600

    Migration to GitHub Actions from Travis CI (#1750)

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ev
 
-cd ${TRAVIS_BUILD_DIR}/scheduler
+cd ${GITHUB_WORKSPACE}/scheduler
 lein deps
 lein uberjar
 VERSION=$(lein print :version | tr -d '"')
 
-cd  ${TRAVIS_BUILD_DIR}/travis
-unzip ${TRAVIS_BUILD_DIR}/scheduler/datomic/datomic-free-0.9.5394.zip
-cp "${TRAVIS_BUILD_DIR}/scheduler/target/cook-${VERSION}.jar" datomic-free-0.9.5394/lib/
+cd  ${GITHUB_WORKSPACE}/travis
+unzip ${GITHUB_WORKSPACE}/scheduler/datomic/datomic-free-0.9.5394.zip
+cp "${GITHUB_WORKSPACE}/scheduler/target/cook-${VERSION}.jar" datomic-free-0.9.5394/lib/
