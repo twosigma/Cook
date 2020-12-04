caa7999139d708ed7b356eb80c2fd890f8f41658
Kevin Tang <kevin.tang@twosigma.com>
2020-12-03 23:53:46 -0600

    remove --user

--- travis/build_cook_executor.sh
+++ travis/build_cook_executor.sh
@@ -3,6 +3,6 @@
 set -ev
 
 cd ${GITHUB_WORKSPACE}/executor
-pip install --user -r requirements.txt
+pip install -r requirements.txt
 ./bin/prepare-executor.sh local ${GITHUB_WORKSPACE}/scheduler/resources/public
 tar -C ${GITHUB_WORKSPACE}/travis -xzf ./dist/cook-executor-local.tar.gz
