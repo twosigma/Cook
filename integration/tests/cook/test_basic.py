41f57754088ca53761fef6b077312899c4b84126
Kathryn Zhou <kathrynzhou@college.harvard.edu>
2020-12-30 15:22:23 -0500

    debugging

--- integration/tests/cook/test_basic.py
+++ integration/tests/cook/test_basic.py
@@ -3444,9 +3444,10 @@ class CookTest(util.CookTest):
             util.kill_jobs(self.cook_url, [job_uuid], assert_response=False)
 
     @unittest.skipUnless(util.docker_tests_enabled(), "Requires we're in an environment that requires docker images.")
+    @unittest.skipUnless(util.using_kubernetes(), 'Test requires kubernetes')
     def test_memory_multiplier(self):
         mem = 64
-        command = f'bash -c \'if [[ "${{COOK_MEMORY_REQUEST_BYTES}}" -eq {mem * 1024 * 1024} ]]; then exit 0; else exit 1; fi\''
+        command = f'bash -c \'echo $(env | sort) ; if [[ "${{COOK_MEMORY_REQUEST_BYTES}}" -eq {mem * 1024 * 1024} ]]; then exit 0; else exit 1; fi\''
         job_uuid, resp = util.submit_job(self.cook_url, command=command, max_retries=5, mem=mem)
         self.assertEqual(resp.status_code, 201, msg=resp.content)
         job = util.wait_for_job_in_statuses(self.cook_url, job_uuid, ['completed'])
