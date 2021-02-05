305b8ca38545a75775ade4273bda7f529a02a266
Daniel Posada <daniel.posada@gmail.com>
2021-02-05 16:26:40 -0600

    Logs exceptions in deep-merge-with (#1786)

--- scheduler/test/cook/test/util.clj
+++ scheduler/test/cook/test/util.clj
@@ -37,3 +37,15 @@
                          :d {:d :d}}
                         {:a {:a :a}
                          :d {:d :e}}))))
+
+(deftest test-deep-merge-with
+  (is (= {:a {:b {:z 3, :c 3, :d {:z 9, :x 1, :y 2}}, :e 103}, :f 4}
+         (deep-merge-with +
+                          {:a {:b {:c 1 :d {:x 1 :y 2}} :e 3} :f 4}
+                          {:a {:b {:c 2 :d {:z 9} :z 3} :e 100}})))
+  (is (= {"foo" 2}
+         (deep-merge-with - {"foo" 3} {"foo" 1})))
+  (is (thrown? NullPointerException
+               (deep-merge-with - {"foo" nil} {"foo" 1})))
+  (is (thrown? NullPointerException
+               (deep-merge-with - {"foo" 1} {"foo" nil}))))
