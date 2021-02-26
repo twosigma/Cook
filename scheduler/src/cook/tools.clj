355f2454a781ffb278b34c235bd56599bfac2b70
Scott Crosby <scrosby@twosigma.com>
2021-02-26 10:05:56 -0600

    BROKEN PATCHES

--- scheduler/src/cook/tools.clj
+++ scheduler/src/cook/tools.clj
@@ -75,8 +75,8 @@
         (let [ignore-group-ids (get-in config/config [:settings :ignore-supplemental-group-ids])
               system-group-ids (retrieve-system-ids "-G" user-name)]
           {:cache-expires-at (-> 30 t/minutes t/from-now)
-           :system-ids (merge ignore-group-ids system-group-ids)}))]
-  (defn user->all-group-ids [user-name]
+           :system-ids (merge (list ignore-group-ids) system-group-ids)}))]
+  (defn user->all-group-ids [user-name])
     "Retrieves the (potentially cached) collection
     of all group ids for the specified user"
     (:system-ids
