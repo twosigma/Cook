70e2ff666e78082323b73a5499565432d8ab5a3d
Scott Crosby <scrosby@twosigma.com>
2021-08-27 12:53:13 -0500

    Update GC log options for JDK11

--- scheduler/project.clj
+++ scheduler/project.clj
@@ -223,6 +223,9 @@
              ;"-Dsun.security.jgss.native=true"
              ;"-Dsun.security.jgss.lib=/opt/mitkrb5/lib/libgssapi_krb5.so"
              ;"-Djavax.security.auth.useSubjectCredsOnly=false"
+             "-Xlog:gc*,compaction*,stringdedup*=debug,stringtable*,ergo*,safepoint,age=trace:file=gclog-%t:time,level,tags"
+             "-XX:+UseG1GC"
+             "-XX:+UseStringDeduplication"
              "-XX:+HeapDumpOnOutOfMemoryError"
              "--illegal-access=warn"])
 ; TODO: Re-add back needed GC options suitable for JDK11.
