d610d4967b06f487b73090991657e898d2fe7757
Daniel Posada <daniel.posada@gmail.com>
2021-08-29 18:13:50 -0500

    Makes test_preemption_basic respect the default submit pool

--- integration/tests/cook/test_multi_user.py
+++ integration/tests/cook/test_multi_user.py
@@ -442,24 +442,20 @@ class MultiUserCookTest(util.CookTest):
                 util.reset_limit(self.cook_url, 'quota', user.name, reason=self.current_name(), pool=pool)
 
     @unittest.skipUnless(util.is_preemption_enabled(), 'Preemption is not enabled on the cluster')
+    @unittest.skipUnless(util.default_submit_pool() is not None, 'Test requires a default test pool')
     @pytest.mark.serial
-    @pytest.mark.xfail
+    # @pytest.mark.xfail
     # The test timeout needs to be a little more than 2 times the
     # rebalancer interval to allow at least two runs of the rebalancer
     @pytest.mark.timeout((util.rebalancer_interval_seconds() * 2.5) + 60)
     def test_preemption_basic(self):
-        self.trigger_preemption(pool=None)
-
-    @unittest.skipUnless(util.is_preemption_enabled(), 'Preemption is not enabled on the cluster')
-    @unittest.skipUnless(util.are_pools_enabled(), 'Pools are not enabled on the cluster')
-    @pytest.mark.serial
-    @pytest.mark.xfail
-    def test_preemption_for_pools(self):
-        pools, _ = util.active_pools(self.cook_url)
-        self.assertLess(0, len(pools))
-        for pool in pools:
-            self.logger.info(f'Triggering preemption for {pool}')
-            self.trigger_preemption(pool=pool['name'])
+        pool = util.default_submit_pool()
+        rebalancer_pool_regex = util.rebalancer_settings().get('pool-regex', None)
+        if rebalancer_pool_regex and re.match(rebalancer_pool_regex, pool):
+            self.trigger_preemption(pool=pool)
+        else:
+            self.skipTest(f'The rebalancer pool regex ({rebalancer_pool_regex}) '
+                          f'does not match the default submit pool ({pool})')
 
     @unittest.skipUnless(util.are_pools_enabled(), "Requires pools")
     @unittest.skipIf(util.using_kubernetes(), 'This test is not yet supported on kubernetes')
