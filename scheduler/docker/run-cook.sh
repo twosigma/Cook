ccfd253d24b2f15642c88de4a206eaf138375279
Scott Crosby <scrosby@twosigma.com>
2021-10-29 13:31:19 -0500

    Don't seed mesos pools anymore in opensource integration tests.

--- scheduler/docker/run-cook.sh
+++ scheduler/docker/run-cook.sh
@@ -9,7 +9,6 @@ echo "Seeding test data..."
 export COOK_DB_TEST_PG_DB="cook_local"
 export COOK_DB_TEST_PG_USER="cook_scheduler"
 export COOK_DB_TEST_PG_SERVER="cook-postgres"
-lein exec -p /opt/cook/datomic/data/seed_pools.clj ${COOK_DATOMIC_URI}
 lein exec -p /opt/cook/datomic/data/seed_k8s_pools.clj ${COOK_DATOMIC_URI}
 lein exec -p /opt/cook/datomic/data/seed_running_jobs.clj ${COOK_DATOMIC_URI}
 lein with-profiles +docker run $1
