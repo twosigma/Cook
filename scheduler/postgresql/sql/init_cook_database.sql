98c9185344e836d85474dbfc6676735809474b7a
Scott Crosby <scrosby@twosigma.com>
2021-12-15 14:15:43 -0600

    Strip off unneeded schema definition tables.

--- scheduler/postgresql/sql/init_cook_database.sql
+++ scheduler/postgresql/sql/init_cook_database.sql
@@ -13,29 +13,6 @@ SET SCHEMA :'cook_schema';
 -- letter in cook_schema. Schema names are lowercased when created, but case-sensitive when
 -- in the search path.
 
---- Setup the tables.
-create table resources (
-   resource_name varchar(30) PRIMARY KEY,
-   resource_description text
-);
-
-create table pools (
-  pool_name varchar(60) PRIMARY KEY,
-  pool_active bool NOT NULL,
-  pool_description text NOT NULL
-);
-
-create table resource_limits (
-  resource_limit_type varchar(8) NOT NULL CHECK (resource_limit_type IN ('quota', 'share')),
-  pool_name varchar(60) NOT NULL,  -- references pool(pool_name) NOT NULL,
-  user_name varchar(60) NOT NULL, -- 'default' is default user.
-  resource_name varchar(30) NOT NULL,
-  amount float NOT NULL,
-  reason text NOT NULL,
--- TODO:  deletion_timestamp, -- NOT NULL means deleted.
-  PRIMARY KEY (resource_limit_type,pool_name,user_name,resource_name)
-);
-
 COMMIT
 
 -- Just show the tables at the end.
