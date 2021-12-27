250955fde83f66ec808b9406a41c24763a1169df
Scott Crosby <scrosby@twosigma.com>
2021-12-27 10:30:33 -0600

    Quota support.

--- scheduler/liquibase/changelog/com/twosigma/cook/changelogs/setup.postgresql.sql
+++ scheduler/liquibase/changelog/com/twosigma/cook/changelogs/setup.postgresql.sql
@@ -8,3 +8,33 @@
 -- create in' when running the first CREATE TABLE. It can be caused by there being a capital
 -- letter in cook_schema. Schema names are lowercased when created, but case-sensitive when
 -- in the search path.
+
+--- Setup the tables.
+--changeset scrosby:2021-12-22-resources
+create table resources (
+   resource_name varchar(30) PRIMARY KEY,
+   resource_description text
+);
+
+--changeset scrosby:2021-12-22-pools
+create table pools (
+  pool_name varchar(60) PRIMARY KEY,
+  pool_active bool NOT NULL,
+  pool_description text NOT NULL
+);
+
+--changeset scrosby:2021-12-22-resource_limits
+create table resource_limits (
+  resource_limit_type varchar(8) NOT NULL CHECK (resource_limit_type IN ('quota', 'share')),
+  pool_name varchar(60) NOT NULL,  -- references pool(pool_name) NOT NULL,
+  user_name varchar(60) NOT NULL, -- 'default' is default user.
+  resource_name varchar(30) NOT NULL,
+  amount float NOT NULL,
+  reason text NOT NULL,
+-- TODO:  deletion_timestamp, -- NOT NULL means deleted.
+  PRIMARY KEY (resource_limit_type,pool_name,user_name,resource_name)
+);
+
+-- Just show the tables at the end.
+--ignoreLines:1
+\dt :cook_schema.*
