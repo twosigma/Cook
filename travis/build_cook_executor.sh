28fbf7c6d85fe1494671345cac22e513d508f60d
Scott Crosby <scrosby@twosigma.com>
2022-01-07 15:33:53 -0600

    Remove all travis scripts.

--- travis/build_cook_executor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-cd ${GITHUB_WORKSPACE}/executor
-pip install -r requirements.txt
-./bin/prepare-executor.sh local ${GITHUB_WORKSPACE}/scheduler/resources/public
-tar -C ${GITHUB_WORKSPACE}/travis -xzf ./dist/cook-executor-local.tar.gz
