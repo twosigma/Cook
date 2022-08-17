f018e53511fee7861ebd4f7deb5bd23a8ff8ed83
Alex Hays <ahaysx@gmail.com>
2022-08-17 17:37:06 -0400

    restructure error conditions in write-hostname-to-datomic

--- scheduler/src/cook/scheduler/scheduler.clj
+++ scheduler/src/cook/scheduler/scheduler.clj
@@ -330,23 +330,24 @@
 
 (defn write-hostname-to-datomic
   "Takes a hostname for an instance and saves it to datomic."
-  [conn task-id hostname]
-  (try
-    (let [db (db conn)
-          _ (when-not task-id
-              (throw (ex-info "task-id is nil. Something unexpected has happened."
-                              {:hostname hostname
-                               :task-id task-id})))
-          [instance] (first (q '[:find ?i
-                                 :in $ ?task-id
-                                 :where
-                                 [?i :instance/task-id ?task-id]]
-                               db task-id))]
-      (if (nil? instance)
-        (log/error "Hostname update error. No instance for task-id" task-id)
-        @(d/transact conn [[:db/add instance :instance/hostname hostname]])))
-    (catch Exception e
-      (log/error e "Hostname update error"))))
+  [conn task-id hostname] 
+  (log-structured/debug "Writing hostname" {:task-id task-id :hostname hostname})
+  (if task-id
+    (try
+      (let [db (db conn)
+            [instance] (first (q '[:find ?i
+                                   :in $ ?task-id
+                                   :where
+                                   [?i :instance/task-id ?task-id]]
+                                 db task-id))]
+        (if (nil? instance)
+          (log-structured/error "Failed to write hostname: no instance for task-id."
+                                {:hostname hostname :task-id task-id})
+          @(d/transact conn [[:db/add instance :instance/hostname hostname]])))
+      (catch Exception e
+        (log/error e "Failed to write hostname: Datomic error")))
+    (log-structured/error "Failed to write hostname: task-id is nil."
+                          {:hostname hostname})))
 
 (defn- task-id->instance-id
   "Retrieves the instance-id given a task-id"
